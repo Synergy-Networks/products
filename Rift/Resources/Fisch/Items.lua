14957321975c77bac3daf11a49f50f2709d3b371
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-02-14 17:48:54 +0000

    Create Items.lua

---
+++ Rift/Resources/Fisch/Items.lua
@@ -0,0 +1,201 @@
+return
+    ["Crab Cage"] = {
+        ["Rarity"] = "Unusual",
+        ["Price"] = 45
+    },
+    ["Firework"] = {
+        ["Rarity"] = "Limited",
+        ["Price"] = 130
+    },
+    ["Conception Conch"] = {
+        ["Rarity"] = "Mythical",
+        ["Price"] = 444
+    },
+    ["Glider"] = {
+        ["Rarity"] = "Rare",
+        ["Price"] = 900,
+        ["OnlyBuyOne"] = true
+    },
+    ["Advanced Glider"] = {
+        ["Rarity"] = "Rare",
+        ["Price"] = 2500,
+        ["OnlyBuyOne"] = true
+    },
+    ["Fish Radar"] = {
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://132119413174655",
+        ["Price"] = 8000,
+        ["OnlyBuyOne"] = true
+    },
+    ["GPS"] = {
+        ["Rarity"] = "Uncommon",
+        ["Icon"] = "rbxassetid://92660360174055",
+        ["Price"] = 100,
+        ["OnlyBuyOne"] = true
+    },
+    ["Tempest Totem"] = {
+        ["Rarity"] = "Rare",
+        ["Price"] = 2000
+    },
+    ["Windset Totem"] = {
+        ["Rarity"] = "Rare",
+        ["Price"] = 2000
+    },
+    ["Sundial Totem"] = {
+        ["Rarity"] = "Rare",
+        ["Price"] = 2000
+    },
+    ["Smokescreen Totem"] = {
+        ["Rarity"] = "Rare",
+        ["Price"] = 2000
+    },
+    ["Aurora Totem"] = {
+        ["Rarity"] = "Mythical",
+        ["Price"] = 500000
+    },
+    ["Meteor Totem"] = {
+        ["Rarity"] = "Legendary",
+        ["Price"] = 75000
+    },
+    ["Eclipse Totem"] = {
+        ["Rarity"] = "Mythical",
+        ["Price"] = 250000
+    },
+    ["Blizzard Totem"] = {
+        ["Rarity"] = "Mythical",
+        ["Price"] = 275000
+    },
+    ["Avalanche Totem"] = {
+        ["Rarity"] = "Mythical",
+        ["Price"] = 150000
+    },
+    ["Witches Ingredient"] = {
+        ["Rarity"] = "Rare",
+        ["Price"] = 10000
+    },
+    ["Water Bubble"] = {
+        ["Rarity"] = "Legendary",
+        ["Price"] = 25000,
+        ["OnlyBuyOne"] = true
+    },
+    ["Basic Diving Gear"] = {
+        ["Rarity"] = "Uncommon",
+        ["Price"] = 3000,
+        ["OnlyBuyOne"] = true
+    },
+    ["Advanced Diving Gear"] = {
+        ["Rarity"] = "Unusual",
+        ["Price"] = 15000,
+        ["OnlyBuyOne"] = true
+    },
+    ["Basic Oxygen Tank"] = {
+        ["Rarity"] = "Uncommon",
+        ["Price"] = 1000,
+        ["OnlyBuyOne"] = true
+    },
+    ["Beginner Oxygen Tank"] = {
+        ["Rarity"] = "Unusual",
+        ["Price"] = 3500,
+        ["OnlyBuyOne"] = true
+    },
+    ["Intermediate Oxygen Tank"] = {
+        ["Rarity"] = "Rare",
+        ["Price"] = 10000,
+        ["OnlyBuyOne"] = true
+    },
+    ["Advanced Oxygen Tank"] = {
+        ["Rarity"] = "Legendary",
+        ["Price"] = 25000,
+        ["OnlyBuyOne"] = true
+    },
+    ["Winter Cloak"] = {
+        ["Rarity"] = "Unusual",
+        ["Price"] = 7500,
+        ["OnlyBuyOne"] = true
+    },
+    ["Flippers"] = {
+        ["Rarity"] = "Unusual",
+        ["Price"] = 9000,
+        ["OnlyBuyOne"] = true
+    },
+    ["Super Flippers"] = {
+        ["Rarity"] = "Legendary",
+        ["Price"] = 30000,
+        ["OnlyBuyOne"] = true
+    },
+    ["Tidebreaker"] = {
+        ["Rarity"] = "Mythical",
+        ["Price"] = 80000,
+        ["OnlyBuyOne"] = true
+    },
+    ["Ancient Thread"] = {
+        ["Rarity"] = "Exotic",
+        ["SellValue"] = 700,
+        ["Price"] = (1 / 0)
+    },
+    ["Magic Thread"] = {
+        ["Rarity"] = "Exotic",
+        ["SellValue"] = 250,
+        ["Price"] = (1 / 0)
+    },
+    ["Lunar Thread"] = {
+        ["Rarity"] = "Exotic",
+        ["SellValue"] = 1250,
+        ["Price"] = (1 / 0)
+    },
+    ["Pickaxe"] = {
+        ["Rarity"] = "Rare",
+        ["Price"] = 5000,
+        ["OnlyBuyOne"] = true
+    },
+    ["Nuke"] = {
+        ["Rarity"] = "Exotic",
+        ["Price"] = (1 / 0)
+    },
+    ["Fillionaire"] = {
+        ["Rarity"] = "Exotic",
+        ["Price"] = (1 / 0)
+    },
+    ["Treasure Map"] = {
+        ["Rarity"] = "Exotic",
+        ["Price"] = (1 / 0)
+    },
+    ["Handwritten Note"] = {
+        ["Rarity"] = "Exotic",
+        ["Price"] = (1 / 0)
+    },
+    ["TNT"] = {
+        ["Rarity"] = "Rare",
+        ["Price"] = (1 / 0),
+        ["purchasable"] = false
+    },
+    ["Heart Of Zeus"] = {
+        ["Rarity"] = "Mythical",
+        ["Price"] = (1 / 0),
+        ["purchasable"] = false
+    },
+    ["Zeus Storm Totem"] = {
+        ["Rarity"] = "Mythical",
+        ["Price"] = 1500000
+    },
+    ["Poseidon Wrath Totem"] = {
+        ["Rarity"] = "Mythical",
+        ["Price"] = 1500000
+    },
+    ["Regular Token"] = {
+        ["Rarity"] = "Exotic",
+        ["Price"] = (1 / 0)
+    },
+    ["Elite Token"] = {
+        ["Rarity"] = "Exotic",
+        ["Price"] = (1 / 0)
+    },
+    ["Bag of Presents"] = {
+        ["Rarity"] = "Exotic",
+        ["Price"] = (1 / 0)
+    },
+    ["Skin Crate"] = {
+        ["Rarity"] = "Exotic",
+        ["Price"] = (1 / 0)
+    }
+}
