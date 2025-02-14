cc00112caf8bb6ca9384cc6333d84cc437a3ba3e
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-02-14 18:49:36 +0000

    Create Rarities.lua

---
+++ Rift/Resources/Fisch/Rarities.lua
@@ -0,0 +1,14 @@
+return {
+    ["Trash"] = Color3.fromRGB(145, 145, 145),
+    ["Common"] = Color3.fromRGB(142, 187, 191),
+    ["Uncommon"] = Color3.fromRGB(161, 255, 169),
+    ["Unusual"] = Color3.fromRGB(192, 135, 198),
+    ["Rare"] = Color3.fromRGB(119, 108, 181),
+    ["Legendary"] = Color3.fromRGB(240, 181, 109),
+    ["Mythical"] = Color3.fromRGB(255, 62, 120),
+    ["Exotic"] = Color3.fromRGB(255, 255, 255),
+    ["Secret"] = Color3.fromRGB(255, 255, 255),
+    ["Limited"] = Color3.fromRGB(255, 130, 57),
+    ["Extinct"] = Color3.fromRGB(54, 73, 159),
+    ["Relic"] = Color3.fromRGB(120, 255, 183)
+}
