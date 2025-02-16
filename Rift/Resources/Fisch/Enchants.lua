fe47ee2ed965c213d19de187359fe0774ab59e68
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-02-16 20:30:14 +0000

    Create Enchants.lua

---
+++ Rift/Resources/Fisch/Enchants.lua
@@ -0,0 +1,242 @@
+return {
+    ["Sea King"] = {
+        ["Description"] = "Makes fish 10% bigger",
+        ["Color"] = Color3.fromRGB(53, 77, 211),
+        ["StrokeColor"] = Color3.fromRGB(9, 13, 35),
+        ["Display"] = "Sea King",
+        ["StatIncrease"] = 10,
+        ["IsExalted"] = false
+    },
+    ["Swift"] = {
+        ["Description"] = "Decreases lure time by 30%",
+        ["Color"] = Color3.fromRGB(181, 227, 229),
+        ["StrokeColor"] = Color3.fromRGB(40, 50, 50),
+        ["Display"] = "Swift",
+        ["StatIncrease"] = 30,
+        ["IsExalted"] = false
+    },
+    ["Long"] = {
+        ["Description"] = "Increases line distance by 50 studs",
+        ["Color"] = Color3.fromRGB(223, 165, 64),
+        ["StrokeColor"] = Color3.fromRGB(33, 25, 14),
+        ["Display"] = "Long",
+        ["StatIncrease"] = 50,
+        ["IsExalted"] = false
+    },
+    ["Ghastly"] = {
+        ["Description"] = "20% chance for a fish to be Translucent",
+        ["Color"] = Color3.fromRGB(129, 223, 173),
+        ["StrokeColor"] = Color3.fromRGB(46, 79, 61),
+        ["Display"] = "Ghastly",
+        ["StatIncrease"] = 20,
+        ["IsExalted"] = false
+    },
+    ["Lucky"] = {
+        ["Description"] = "Increases luck by 20%",
+        ["Color"] = Color3.fromRGB(126, 229, 164),
+        ["StrokeColor"] = Color3.fromRGB(25, 45, 32),
+        ["Display"] = "Lucky",
+        ["StatIncrease"] = 20,
+        ["IsExalted"] = false
+    },
+    ["Divine"] = {
+        ["Description"] = "Increases luck by 45%",
+        ["Color"] = Color3.fromRGB(198, 229, 220),
+        ["StrokeColor"] = Color3.fromRGB(118, 135, 132),
+        ["Display"] = "Divine",
+        ["StatIncrease"] = 45,
+        ["IsExalted"] = false
+    },
+    ["Mutated"] = {
+        ["Description"] = "Increases mutation luck by 7%",
+        ["Color"] = Color3.fromRGB(147, 229, 112),
+        ["StrokeColor"] = Color3.fromRGB(45, 58, 39),
+        ["Display"] = "Mutated",
+        ["StatIncrease"] = 7,
+        ["IsExalted"] = false
+    },
+    ["Unbreakable"] = {
+        ["Description"] = "Increases strength by 10,000kg",
+        ["Color"] = Color3.fromRGB(225, 179, 229),
+        ["StrokeColor"] = Color3.fromRGB(16, 13, 16),
+        ["Display"] = "Unbreakable",
+        ["StatIncrease"] = 10000,
+        ["IsExalted"] = false
+    },
+    ["Herculean"] = {
+        ["Description"] = "Increases strength by 25,000kg",
+        ["Color"] = Color3.fromRGB(255, 234, 0),
+        ["StrokeColor"] = Color3.fromRGB(48, 43, 0),
+        ["Display"] = "Herculean",
+        ["StatIncrease"] = 25000,
+        ["IsExalted"] = false
+    },
+    ["Steady"] = {
+        ["Description"] = "Reduces progress loss by 25% when fishing",
+        ["Color"] = Color3.fromRGB(229, 207, 191),
+        ["StrokeColor"] = Color3.fromRGB(21, 19, 18),
+        ["Display"] = "Steady",
+        ["StatIncrease"] = 25,
+        ["IsExalted"] = false
+    },
+    ["Blessed"] = {
+        ["Description"] = "Increases shiny & sparkling chances by 2%",
+        ["Color"] = Color3.fromRGB(255, 79, 164),
+        ["StrokeColor"] = Color3.fromRGB(42, 20, 34),
+        ["Display"] = "Blessed",
+        ["StatIncrease"] = 2,
+        ["IsExalted"] = false
+    },
+    ["Wormhole"] = {
+        ["Description"] = "45% chance to catch a fish from a random place",
+        ["Color"] = Color3.fromRGB(145, 90, 255),
+        ["StrokeColor"] = Color3.fromRGB(13, 8, 22),
+        ["Display"] = "Wormhole",
+        ["StatIncrease"] = 45,
+        ["IsExalted"] = false
+    },
+    ["Resilient"] = {
+        ["Description"] = "Increased resilience by 35%",
+        ["Color"] = Color3.fromRGB(106, 255, 198),
+        ["StrokeColor"] = Color3.fromRGB(17, 29, 25),
+        ["Display"] = "Resilient",
+        ["StatIncrease"] = 35,
+        ["IsExalted"] = false
+    },
+    ["Controlled"] = {
+        ["Description"] = "Increased rod control bar size",
+        ["Color"] = Color3.fromRGB(175, 155, 255),
+        ["StrokeColor"] = Color3.fromRGB(32, 28, 39),
+        ["Display"] = "Controlled",
+        ["StatIncrease"] = 0.05,
+        ["IsExalted"] = false
+    },
+    ["Storming"] = {
+        ["Description"] = "Luck is increased by 95% during a rainy day",
+        ["Color"] = Color3.fromRGB(255, 236, 131),
+        ["StrokeColor"] = Color3.fromRGB(36, 36, 24),
+        ["Display"] = "Storming",
+        ["StatIncrease"] = 95,
+        ["IsExalted"] = false
+    },
+    ["Scrapper"] = {
+        ["Description"] = "60% chance to not consume bait",
+        ["Color"] = Color3.fromRGB(255, 155, 83),
+        ["StrokeColor"] = Color3.fromRGB(36, 23, 13),
+        ["Display"] = "Scrapper",
+        ["StatIncrease"] = 60,
+        ["IsExalted"] = false
+    },
+    ["Breezed"] = {
+        ["Description"] = "Luck is increased by 65% during a windy day",
+        ["Color"] = Color3.fromRGB(188, 208, 255),
+        ["StrokeColor"] = Color3.fromRGB(28, 30, 36),
+        ["Display"] = "Breezed",
+        ["StatIncrease"] = 65,
+        ["IsExalted"] = false
+    },
+    ["Insight"] = {
+        ["Description"] = "1.5X XP after catching a fish",
+        ["Color"] = Color3.fromRGB(128, 255, 114),
+        ["StrokeColor"] = Color3.fromRGB(29, 36, 27),
+        ["Display"] = "Insight",
+        ["StatIncrease"] = 1.5,
+        ["IsExalted"] = false
+    },
+    ["Clever"] = {
+        ["Description"] = "2.25X XP after catching a fish",
+        ["Color"] = Color3.fromRGB(255, 110, 166),
+        ["StrokeColor"] = Color3.fromRGB(36, 18, 24),
+        ["Display"] = "Clever",
+        ["StatIncrease"] = 2.25,
+        ["IsExalted"] = false
+    },
+    ["Noir"] = {
+        ["Description"] = "Caught fish have a 50% chance to be Albino or Darkened.",
+        ["Color"] = Color3.fromRGB(255, 255, 255),
+        ["StrokeColor"] = Color3.fromRGB(0, 0, 0),
+        ["Display"] = "Noir",
+        ["StatIncrease"] = 50,
+        ["IsExalted"] = false
+    },
+    ["Hasty"] = {
+        ["Description"] = "Decreases lure time by 55%",
+        ["Color"] = Color3.fromRGB(229, 209, 92),
+        ["StrokeColor"] = Color3.fromRGB(50, 42, 12),
+        ["Display"] = "Hasty",
+        ["StatIncrease"] = 55,
+        ["IsExalted"] = false
+    },
+    ["Quality"] = {
+        ["Description"] = "Increases luck by 15%, Resilience by 5%, and Lure Speed by 15%",
+        ["Color"] = Color3.fromRGB(158, 255, 55),
+        ["StrokeColor"] = Color3.fromRGB(56, 90, 19),
+        ["Display"] = "Quality",
+        ["StatIncrease"] = 0,
+        ["IsExalted"] = false
+    },
+    ["Abyssal"] = {
+        ["Description"] = "Caught fish have a 10% chance to be Abyssal.",
+        ["Color"] = Color3.fromRGB(39, 57, 173),
+        ["StrokeColor"] = Color3.fromRGB(83, 41, 173),
+        ["Display"] = "Abyssal",
+        ["StatIncrease"] = 10,
+        ["IsExalted"] = false
+    },
+    ["Mystical"] = {
+        ["Description"] = "Increased resilience by 45%",
+        ["Color"] = Color3.fromRGB(194, 223, 255),
+        ["StrokeColor"] = Color3.fromRGB(43, 50, 57),
+        ["Display"] = "Mystical",
+        ["StatIncrease"] = 45,
+        ["IsExalted"] = true
+    },
+    ["Quantum"] = {
+        ["Description"] = "Caught fish have a 25% chance to be Subspace.",
+        ["Color"] = Color3.fromRGB(255, 0, 179),
+        ["StrokeColor"] = Color3.fromRGB(49, 0, 36),
+        ["Display"] = "Quantum",
+        ["StatIncrease"] = 25,
+        ["IsExalted"] = true
+    },
+    ["Invincible"] = {
+        ["Description"] = "Allows fishing in any body of liquid.",
+        ["Color"] = Color3.fromRGB(255, 98, 25),
+        ["StrokeColor"] = Color3.fromRGB(35, 13, 3),
+        ["Display"] = "Invincible",
+        ["StatIncrease"] = 0,
+        ["IsExalted"] = true
+    },
+    ["Piercing"] = {
+        ["Description"] = "Rod has a chance to stab fish while reeling.",
+        ["Color"] = Color3.fromRGB(13, 167, 141),
+        ["StrokeColor"] = Color3.fromRGB(3, 40, 33),
+        ["Display"] = "Piercing",
+        ["StatIncrease"] = 0,
+        ["IsExalted"] = true
+    },
+    ["Anomalous"] = {
+        ["Description"] = "Caught fish have a 10% chance to be duplicated and Anomalous",
+        ["Color"] = Color3.fromRGB(255, 29, 29),
+        ["StrokeColor"] = Color3.fromRGB(35, 4, 4),
+        ["Display"] = "Anomalous",
+        ["StatIncrease"] = 0,
+        ["IsExalted"] = true
+    },
+    ["Sea Overlord"] = {
+        ["Description"] = "Makes fish 25% bigger",
+        ["Color"] = Color3.fromRGB(51, 168, 211),
+        ["StrokeColor"] = Color3.fromRGB(9, 29, 35),
+        ["Display"] = "Sea Overlord",
+        ["StatIncrease"] = 25,
+        ["IsExalted"] = true
+    },
+    ["Immortal"] = {
+        ["Description"] = "Increases luck by 75%",
+        ["Color"] = Color3.fromRGB(232, 217, 255),
+        ["StrokeColor"] = Color3.fromRGB(69, 65, 76),
+        ["Display"] = "Immortal",
+        ["StatIncrease"] = 75,
+        ["IsExalted"] = true
+    }
+}
