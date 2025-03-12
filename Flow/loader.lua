a1b9f2964d1d34f617e50aa612b3a364815890ba
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-03-12 17:24:23 +0000

    Update loader.lua

--- Flow/loader.lua
+++ Flow/loader.lua
@@ -1,10 +1,17 @@
-local StarterGui = cloneref(game:GetService("StarterGui"))
+repeat wait() until game:IsLoaded()
 
-StarterGui:SetCore("SendNotification", {
-    Title = "We've moved!",
-    Text = "Please execute the script copied to your clipboard to continue.",
-    Duration = 10
+local Services = setmetatable({}, {
+    __index = function(_, serviceName)
+        return cloneref(game:GetService(serviceName))
+    end
 })
 
-setclipboard([[loadstring(game:HttpGet("https://github.com/Synergy-Networks/products/raw/main/Pulse/loader.lua", true))()]])
+local starterGui = Services.StarterGui
 
+setclipboard([[loadstring(game:HttpGet("https://github.com/Synergy-Networks/products/raw/main/Rift/loader.lua"))()]])
+
+starterGui:SetCore("SendNotification", {
+    Title = "Read me",
+    Text = "Pulse has been moved to a new loadstring. Execute the loadstring pasted on your clipboard.",
+    Duration = 5
+})
