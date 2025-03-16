3c5acab79ebc8886a9f78c54df08c38d8a9a71e7
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-03-16 20:29:29 +0000

    Update Manual.lua

--- Rift/Resources/Fisch/Manual.lua
+++ Rift/Resources/Fisch/Manual.lua
@@ -1,5 +1,10 @@
 return {
     WorldEvents = {
+        "Rowdy McCharm",
+        "O'Mango Goldgrin",
+        "Sunny O'Coin",
+        "Blarney McBreeze",
+        "Plumrick O'Luck",
         "Whales Pool",
         "Forsaken Veil - Scylla",
         "Lovestorm Eel",
