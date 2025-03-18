1bfac16464c98cdbdefc9fa5b903cc958188e10f
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-03-18 19:40:02 +0000

    Update loader.lua

--- Rift/loader.lua
+++ Rift/loader.lua
@@ -1,1 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/refs/heads/main/Rift/Games/Fisch.lua",true))()
+if game.GameId == 5750914919 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/refs/heads/main/Rift/Games/Fisch.lua",true))()
+elseif game.GameId == 6331902150 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/refs/heads/main/Rift/Games/Forsaken.lua",true))()
+end
