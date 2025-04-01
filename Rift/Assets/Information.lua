32e568aed3bcbb6eec6424ca6fd77bfbc5d36731
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-04-01 16:25:28 +0000

    Create Information.lua

---
+++ Rift/Assets/Information.lua
@@ -0,0 +1,16 @@
+return {
+    VERSION = "1.24.3.1",
+    CHANNEL = "Beta",
+    ADVERTISEMENT_ENABLED = true,
+    ADVERTISEMENTS = {
+        "Come check out our FREE Chat bypasser (BetterBypasser) fully equipped with handful of trolling tools and even a AI Chatbot hub.",
+        "You can showcase Rift and receive a free permanent key! More details available at our Discord server.",
+        "We want your feedback and for you to report any issues you have with Rift. Please send your feedback to our Discord.",
+        "Some games may not be fully updated to be compatible with Rift. If this is the case, don't hesitate to report this on our Discord server.",
+        "Want more scripts like this? Go to our free Discord server and get access to a wide range of trolling tools and AI Chatbot hub!",
+        "There may be problems with Rift as it is in Beta. If this is the case, please report this on our Discord server.",
+        "Join our Discord server to get help and get the latest updates on Rift and other services.",
+        "Want to be up to date with the latest Roblox exploiting news? Go to our free Discord server and get the News Notifications role.",
+        "We hope that you're enjoying your experience with Rift. We hope to see you again!"
+    }
+}
