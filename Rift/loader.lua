ce31d4c89f186737a0a6fe9a36b5f17e9a4ff61b
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-04-03 16:48:20 +0000

    Update loader.lua

--- Rift/loader.lua
+++ Rift/loader.lua
@@ -1,3 +1,7 @@
+if identifyexecutor:lower():find("Solara") then
+    game.Players.LocalPlayer:Kick("This executor is too shit. Switch to another executor to continue using Rift.")
+end
+
 if game.PlaceId == 16732694052 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/refs/heads/main/Rift/Games/FischFirstSea.lua",true))()
 elseif game.PlaceId == 72907489978215 then
