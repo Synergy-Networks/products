2bba6a4b778cddfda6c6db69e0da1c27979646fc
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-04-09 19:11:54 +0000

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -1,8 +1,8 @@
 return {
     VERSION = "1.25.6",
     CHANNEL = "Beta",
-    ADVERTISEMENT_ENABLED = false,
+    ADVERTISEMENT_ENABLED = true,
     ADVERTISEMENTS = {
-        "<b><font color='rgb(255,180,60)'>Rift</font></b> can be <i>keyless</i> — enjoy uninterrupted access for just <b><font color='rgb(60,255,60)'>$3.75</font></b> via <b>Crypto</b>.<br>Buy now at <font color='rgb(100,200,255)'>shoppy.gg/@SynergyNetworks</font>"
+        "<b><font color='#FFB43C'>Rift</font></b> can be <i>keyless</i> — enjoy uninterrupted access for just <b><font color='#3CFF3C'>$3.75</font></b> via <b>Crypto</b>.\nBuy now at <font color='#64C8FF'>shoppy.gg/@SynergyNetworks</font>"
     }
 }
