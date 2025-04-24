41299dd7274d0ef26a76441f4dc71d94ab4b316d
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-04-24 19:49:17 +0000

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -2,8 +2,8 @@ return {
     VERSION = "1.25.6.8",
     CHANNEL = "Beta",
     ADVERTISEMENT_ENABLED = true,
-    CHANGELOGS = "<b><u>All Games</u></b>\n• Added a 'Home' tab\n• Migrated to new library version\nAdvertisements will no longer show if you have paid for a key",
-    GAMES_SUPPORTED = "• Dead Rails\n• Fisch\n• Forsaken\n\nExecuting Rift outside of these games will not work.",
+    CHANGELOGS = "<b><u>All Games</u></b>\n• Added a 'Home' tab\n• Migrated to new library version\n• Advertisements will no longer show if you have paid for a key",
+    GAMES_SUPPORTED = "• <b>Dead Rails</b>\n• <b>Fisch</b>\n• <b>Forsaken</b>\n\nExecuting Rift outside of these games will not work."
     ADVERTISEMENTS = {
         "Rift can be keyless — enjoy access for just $3.75 via Crypto. Buy now at https://rifton.top"
     }
