9c8cc9b3dbe0851a6f949b4878038e86736f53b9
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-04-25 17:10:53 +0000

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -3,13 +3,13 @@ return {
     CHANNEL = "Beta",
     ADVERTISEMENT_ENABLED = true,
     CHANGELOGS = "<b><u>All Games</u></b>\n• Added a 'Home' tab\n• Migrated to new library version\n• Advertisements will no longer show if you have paid for a key",
-    GAMES_SUPPORTED =   "<font color=\"#00ff00\">◉</font> = Maintained & Updated\n" ..
-                        "<font color=\"#ffff00\">◉</font> = Experimental / Has Issues\n" ..
-                        "<font color=\"#ff0000\">◉</font> = Offline / Broken\n\n" ..
-                        "• <font color=\"#00ff00\">◉</font> <b>Dead Rails</b>\n" ..
-                        "• <font color=\"#00ff00\">◉</font> <b>Fisch</b>\n" ..
-                        "• <font color=\"#00ff00\">◉</font> <b>Forsaken</b>\n\n" ..
-                        "Executing Rift outside of these games will not work.",
+    GAMES_SUPPORTED =   "<font color=\"#00ff00\">◉</font> → Maintained & Updated\n" ..
+                        "<font color=\"#ffff00\">◉</font> → Experimental / Has Issues\n" ..
+                        "<font color=\"#ff0000\">◉</font> → Offline / Broken\n\n" ..
+                        "• <b>Dead Rails</b> <font color=\"#00ff00\">◉</font>\n" ..
+                        "• <b>Fisch</b> <font color=\"#00ff00\">◉</font>\n" ..
+                        "• <b>Forsaken</b> <font color=\"#00ff00\">◉</font>\n\n" ..
+                        "Executing Rift outside of these games will not work."
     ADVERTISEMENTS = {
         "Rift can be keyless — enjoy access for just $3.75 via Crypto. Buy now at https://rifton.top"
     }
