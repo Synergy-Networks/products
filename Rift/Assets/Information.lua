b3c0cf1138129e53314d810b6d0eea2f669698e5
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-04-27 17:11:22 +0000

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -1,17 +1,26 @@
 return {
-    VERSION = "1.25.6.8",
-    CHANNEL = "Beta",
-    ADVERTISEMENT_ENABLED = true,
-    CHANGELOGS = "<b><u>All Games</u></b>\n• Added a 'Home' tab\n• Migrated to new library version\n• Advertisements will no longer show if you have paid for a key\n• Added Watermark option that is visible by default for PC users only, you can disable it in the settings\n\n<b><u>Dead Rails</u></b>\n• Added Banjo section with Auto Heal and Mute Audio\n• Fixed issues with Auto Finish/Collect Bonds",
-    GAMES_SUPPORTED =   "<font color=\"#00ff00\">◉</font> → Maintained & Updated\n" ..
+	VERSION = "1.25.6.8",
+	CHANNEL = "Beta",
+	ADVERTISEMENT_ENABLED = true,
+	CHANGELOGS = "<b><u>All Games</u></b>\n• Added a 'Home' tab\n• Migrated to new library version\n• Advertisements will no longer show if you have paid for a key\n• Added Watermark option that is visible by default for PC users only, you can disable it in the settings\n\n<b><u>Dead Rails</u></b>\n• Added Banjo section with Auto Heal and Mute Audio\n• Fixed issues with Auto Finish/Collect Bonds\n\n<b><u>Fisch</u></b>\n• Fixed Auto Balance Nuke breaking Auto Cast\n• Fixed issues and improved logic for Auto Balance Nuke",
+	GAMES_SUPPORTED =   "<font color=\"#00ff00\">◉</font> → Maintained & Updated\n" ..
                         "<font color=\"#ffff00\">◉</font> → Experimental / Has Issues\n" ..
                         "<font color=\"#ff0000\">◉</font> → Offline / Broken\n\n" ..
                         "<font color=\"#00ff00\">◉</font> <b>Dead Rails</b>\n" ..
                         "<font color=\"#00ff00\">◉</font> <b>Fisch</b>\n" ..
                         "<font color=\"#00ff00\">◉</font> <b>Forsaken</b>\n\n" ..
                         "Executing Rift outside of these games will not work.",
-
-    ADVERTISEMENTS = {
-        "Rift can be keyless — enjoy access for just $3.75 via Crypto. Buy now at https://rifton.top"
-    }
+	GAME_NAMES = {
+		"Dead Rails",
+		"Fisch",
+		"Forsaken"
+	},
+	GAME_TELEPORTS = {
+		["Dead Rails"] = 70876832253163,
+		["Fisch"] = 16732694052,
+		["Forsaken"] = 18687417158
+	},
+	ADVERTISEMENTS = {
+		"Rift can be keyless — enjoy access for just $3.75 via Crypto. Buy now at https://rifton.top"
+	}
 }
