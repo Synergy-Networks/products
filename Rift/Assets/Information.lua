627163fe87f9e5cb20896e0ea42bd03dd64a8fdd
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-04-27 15:43:40 +0000

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -2,7 +2,7 @@ return {
     VERSION = "1.25.6.8",
     CHANNEL = "Beta",
     ADVERTISEMENT_ENABLED = true,
-    CHANGELOGS = "<b><u>All Games</u></b>\n• Added a 'Home' tab\n• Migrated to new library version\n• Advertisements will no longer show if you have paid for a key",
+    CHANGELOGS = "<b><u>All Games</u></b>\n• Added a 'Home' tab\n• Migrated to new library version\n• Advertisements will no longer show if you have paid for a key\n• Added Watermark option that is visible by default for PC users only, you can disable it in the settings\n\n<b><u>Dead Rails</u></b>\n• Added Banjo section with Auto Heal and Mute Audio\n• Fixed issues with Auto Finish/Collect Bonds",
     GAMES_SUPPORTED =   "<font color=\"#00ff00\">◉</font> → Maintained & Updated\n" ..
                         "<font color=\"#ffff00\">◉</font> → Experimental / Has Issues\n" ..
                         "<font color=\"#ff0000\">◉</font> → Offline / Broken\n\n" ..
