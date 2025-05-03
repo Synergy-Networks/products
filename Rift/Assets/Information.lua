b44c23598106b4b920079d8ebd7fbe003a02f523
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-05-03 17:05:00 +0000

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -1,8 +1,8 @@
 return {
-	VERSION = "1.25.7",
+	VERSION = "1.25.7.1",
 	CHANNEL = "Beta",
 	ADVERTISEMENT_ENABLED = true,
-	CHANGELOGS = "<b><u>All Games</u></b>\n• Added a 'Home' tab\n• Migrated to new library version\n• Advertisements will no longer show if you have paid for a key\n• Added Watermark option that is visible by default for PC users only, you can disable it in the settings\n\n<b><u>Dead Rails</u></b>\n• Added Banjo section with Auto Heal and Mute Audio\n• Fixed issues with Auto Finish/Collect Bonds\n\n<b><u>Fisch</u></b>\n• Fixed Auto Balance Nuke breaking Auto Cast\n• Fixed issues and improved logic for Auto Balance Nuke",
+	CHANGELOGS = [[<b>Dead Rails</b><br/>• Fixed Rift not executing properly<br/>• Rift now adaptively searches for remotes<br/>• Removed Train module until further notice]],
 	GAMES_SUPPORTED =   "<font color=\"#00ff00\">◉</font> → Maintained & Updated\n" ..
                         "<font color=\"#ffff00\">◉</font> → Experimental / Has Issues\n" ..
                         "<font color=\"#ff0000\">◉</font> → Offline / Broken\n\n" ..
