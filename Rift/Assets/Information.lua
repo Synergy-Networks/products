d9118ccc296ca5f73e6b79159f323bb8b6368697
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-05-11 16:39:29 +0000

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -3,15 +3,15 @@ return {
 	CHANNEL = "Beta",
 	ADVERTISEMENT_ENABLED = true,
 	CHANGELOGS = "<b>All Games</b><br/>" ..
-"• Added Execute On Teleport<br/>" ..
-"• Added Prefer Checkbox Toggles<br/>" ..
-"• Added documentation for Rift<br/><br/>" ..
-"<b>Dead Rails</b><br/>" ..
-"• Added new Auto Bonds autofarm<br/>" ..
-"• Added Teleports module<br/>" ..
-"• Added support to Lobby with a Auto Create Lobby option<br/>" ..
-"• Re-added back Train module<br/>" ..
-"• Optimised some things",
+		"• Added Execute On Teleport<br/>" ..
+		"• Added Prefer Checkbox Toggles<br/>" ..
+		"• Added documentation for Rift<br/><br/>" ..
+		"<b>Dead Rails</b><br/>" ..
+		"• Added new Auto Bonds autofarm<br/>" ..
+		"• Added Teleports module<br/>" ..
+		"• Added support to Lobby with a Auto Create Lobby option<br/>" ..
+		"• Re-added back Train module<br/>" ..
+		"• Optimised some things",
 	GAMES_SUPPORTED =   "<font color=\"#00ff00\">◉</font> → Maintained & Updated\n" ..
                         "<font color=\"#ffff00\">◉</font> → Experimental / Has Issues\n" ..
                         "<font color=\"#ff0000\">◉</font> → Offline / Broken\n\n" ..
@@ -43,8 +43,34 @@ loadstring(game:HttpGet("https://github.com/Synergy-Networks/products/raw/main/B
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/OpenSource/main/rizzler.lua"))()]]
 	},
 	ADVERTISEMENTS = {
-		"Rift can be keyless — enjoy access for just $3.75 via Crypto. Buy now at https://rifton.top",
-		"We'd appreciate it if you could give feedback to us! Check out our Discord at https://vaultcord.win/synergy",
-		"Wanna get rid of these ads? Pay $3.75 via Crypto which comes with a keyless version aswell. Buy now at https://rifton.top"
+	    "Rift can be keyless — enjoy access for just $3.75 via Crypto. Buy now at https://rifton.top",
+	    "We'd appreciate it if you could give feedback to us! Check out our Discord at https://vaultcord.win/synergy",
+	    "Wanna get rid of these ads? Pay $3.75 via Crypto which comes with a keyless version aswell. Buy now at https://rifton.top",
+	    "Get exclusive access with Rift — Keyless version available for only $3.75 via Crypto! Visit https://rifton.top",
+	    "Join our growing community on Discord! Share your thoughts at https://vaultcord.win/synergy",
+	    "Unlock the power of Rift without a key! Just $3.75 via Crypto, buy now at https://rifton.top",
+	    "No keys, no limits! Get Rift for $3.75 via Crypto today! Visit https://rifton.top",
+	    "Support our development with your feedback on Discord! Join us at https://vaultcord.win/synergy",
+	    "Upgrade to the keyless version of Rift now for just $3.75 via Crypto! Head to https://rifton.top",
+	    "Rift is now keyless! Get it today for only $3.75 via Crypto. Purchase at https://rifton.top",
+	    "Want to support us? Buy Rift keyless for $3.75 via Crypto and remove ads. Visit https://rifton.top",
+	    "Rift just got better! Access it keyless for $3.75 via Crypto. Buy now at https://rifton.top",
+	    "Get Rift without any ads! Just $3.75 via Crypto for a keyless experience. Buy now at https://rifton.top",
+	    "Your feedback means a lot to us! Join our Discord at https://vaultcord.win/synergy",
+	    "Looking for more? Buy Rift keyless for $3.75 via Crypto and enjoy uninterrupted access! Visit https://rifton.top",
+	    "Get your keyless Rift now for just $3.75 via Crypto. Say goodbye to ads! Visit https://rifton.top",
+	    "Rift without limits! Buy keyless for $3.75 via Crypto now at https://rifton.top",
+	    "Want to get rid of ads? Purchase Rift keyless for $3.75 via Crypto at https://rifton.top",
+	    "Support Rift and get rid of ads for $3.75 via Crypto. Buy now at https://rifton.top",
+	    "Enjoy Rift with no ads — just $3.75 via Crypto for keyless access! Visit https://rifton.top",
+	    "Get the best of Rift! Keyless version now available for $3.75 via Crypto at https://rifton.top",
+	    "Rift is waiting for you! No key required for $3.75 via Crypto. Buy now at https://rifton.top",
+	    "Experience Rift with no interruptions! Buy keyless for $3.75 via Crypto at https://rifton.top",
+	    "Take your Rift experience to the next level — keyless access for only $3.75 via Crypto. Buy now at https://rifton.top",
+	    "Join the Rift community on Discord! We value your feedback at https://vaultcord.win/synergy",
+	    "Enjoy Rift with no ads — keyless access now available for $3.75 via Crypto. Visit https://rifton.top",
+	    "Support Rift development and get keyless access for just $3.75 via Crypto. Buy now at https://rifton.top",
+	    "Want Rift with no ads? Get the keyless version for just $3.75 via Crypto. Visit https://rifton.top"
 	}
+
 }
