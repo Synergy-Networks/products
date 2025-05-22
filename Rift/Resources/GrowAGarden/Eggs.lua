fc9f57744ab1a3c5a3ae5e4833cacac1b1954acc
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-05-22 21:17:06 +0000

    Create Eggs.lua

---
+++ Rift/Resources/GrowAGarden/Eggs.lua
@@ -0,0 +1,57 @@
+local v2 = {
+	["Common Egg"] = {
+		["EggName"] = "Common Egg",
+		["EggRarity"] = "Common",
+		["StockChance"] = 1,
+		["Price"] = 50000,
+		["PurchaseID"] = 3276346455,
+		["LayoutOrder"] = 1,
+		["PetEggDescription"] = "It\'s a dog"
+	},
+	["Uncommon Egg"] = {
+		["EggName"] = "Uncommon Egg",
+		["EggRarity"] = "Uncommon",
+		["StockChance"] = 6,
+		["Price"] = 150000,
+		["PurchaseID"] = 3276346509,
+		["LayoutOrder"] = 1,
+		["PetEggDescription"] = "It\'s a dog"
+	},
+	["Rare Egg"] = {
+		["EggName"] = "Rare Egg",
+		["EggRarity"] = "Rare",
+		["StockChance"] = 20,
+		["Price"] = 600000,
+		["PurchaseID"] = 3276346557,
+		["LayoutOrder"] = 1,
+		["PetEggDescription"] = "It\'s a dog"
+	},
+	["Legendary Egg"] = {
+		["EggName"] = "Legendary Egg",
+		["EggRarity"] = "Legendary",
+		["StockChance"] = 50,
+		["Price"] = 3000000,
+		["PurchaseID"] = 3276346676,
+		["LayoutOrder"] = 1,
+		["PetEggDescription"] = "It\'s a dog"
+	},
+	["Mythical Egg"] = {
+		["EggName"] = "Mythical Egg",
+		["EggRarity"] = "Mythical",
+		["StockChance"] = 70,
+		["Price"] = 8000000,
+		["PurchaseID"] = 3286560171,
+		["LayoutOrder"] = 1,
+		["PetEggDescription"] = "It\'s a dog"
+	},
+	["Bug Egg"] = {
+		["EggName"] = "Bug Egg",
+		["EggRarity"] = "Divine",
+		["StockChance"] = 100,
+		["Price"] = 50000000,
+		["PurchaseID"] = 3277000452,
+		["LayoutOrder"] = 1,
+		["PetEggDescription"] = "It\'s a dog"
+	}
+}
+return v2
