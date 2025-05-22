c08965322e50397d182667cdbf1beedfd6f9f5d1
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-05-22 20:39:14 +0000

    Update and rename Mutations.lua to Tiers.lua

---
+++ Rift/Resources/GrowAGarden/Tiers.lua
@@ -0,0 +1,17 @@
+return {
+	Mutations = {
+		"Shocked",
+		"Twisted",
+		"Wet",
+		"Chilled",
+		"Frozen",
+		"Disco",
+		"Choc",
+		"Burnt"
+	},
+	Variants = {
+		"Normal",
+		"Gold",
+		"Rainbow"
+	}
+}
