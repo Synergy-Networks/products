c525427432b9f9e0076c178007642f4c03a071ea
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-05-25 22:39:59 +0000

    Update Manual.lua

--- Rift/Resources/GrowAGarden/Manual.lua
+++ Rift/Resources/GrowAGarden/Manual.lua
@@ -11,7 +11,7 @@ return {
     TeleportLocations = {
         ["Garden"] = CFrame.new(0, 0, 0),
         ["Seeds Shop"] = CFrame.new(62, 3, -27),
-        ["Sell Shop"] = CFrame.new(-259, 3, -9)
+        ["Sell Shop"] = CFrame.new(-259, 3, -9),
         ["Gear Shop"] = CFrame.new(-285, 3, -33),
         ["Cosmetics Shop"] = CFrame.new(-285, 3, -15),
         ["Eggs Shop"] = CFrame.new(-284, 3, -2),
