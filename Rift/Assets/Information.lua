4690751bf6ed3f8ad0bf6cdbec4c352b97848f5f
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-06-03 21:22:32 +0000

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -1,5 +1,5 @@
 return {
-	VERSION = "1.25.9.1",
+	VERSION = "1.25.10",
 	CHANNEL = "Beta",
 	ADVERTISEMENT_ENABLED = false,
 	CHANGELOGS = "View the change logs at https://docs.rifton.top/changelogs.",
