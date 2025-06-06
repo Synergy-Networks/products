962f546f41a6286358d4de84186906af7453a7a5
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-06-06 11:30:42 +0000

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -1,7 +1,7 @@
 return {
 	VERSION = "1.25.10",
 	CHANNEL = "Beta",
-	ADVERTISEMENT_ENABLED = false,
+	ADVERTISEMENT_ENABLED = true,
 	CHANGELOGS = "View the change logs at https://docs.rifton.top/changelogs.",
 	GAMES_SUPPORTED =  "<font color=\"#00ff00\">◉</font> → Maintained & Updated\n" ..
 			"<font color=\"#ffff00\">◉</font> → Experimental / Has Issues\n" ..
