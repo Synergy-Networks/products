d110b3d0ac5f3484a0a144043c7be6c1e4747dab
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-06-14 15:17:27 +0000

    Update Manual.lua

--- Rift/Resources/GrowAGarden/Manual.lua
+++ Rift/Resources/GrowAGarden/Manual.lua
@@ -1,5 +1,5 @@
 return {
-    Mutations = {
+	Mutations = {
 		"Shocked",
 		"Twisted",
 		"Wet",
@@ -30,36 +30,36 @@ return {
 		"Gold",
 		"Rainbow"
 	},
-    Teleports = {
-        "Seeds Shop",
-        "Sell Shop",
-        "Gear Shop",
-        "Cosmetics Shop",
-        "Eggs Shop",
-        "Night Event Area"
-    },
-    ServerEvents = {
-        ["Disco"] = "DiscoEvent",
-        ["Rain"] = "RainEvent",
-        ["Thunderstorm"] = "Thunderstorm",
-        ["Frost"] = "FrostEvent",
-        ["Blood Moon"] = "BloodMoonEvent",
-        ["Scheckle Rain"] = "ScheckleRain",
-        ["Meteor Shower"] = "MeteorShower",
-        ["Chocolate Rain"] = "ChocolateRain",
-        ["Janzen Storm"] = "JanzenStorm",
-        ["DJJhai"] = "DJJhai",
-        ["Tornado"] = "TornadoEvent",
-        ["Black Hole"] = "Blackhole",
-        ["Bee Swarm"] = "BeeSwarm",
-    },
-    TeleportLocations = {
-        ["Garden"] = CFrame.new(0, 0, 0),
-        ["Seeds Shop"] = CFrame.new(62, 3, -27),
-        ["Sell Shop"] = CFrame.new(-259, 3, -9),
-        ["Gear Shop"] = CFrame.new(-285, 3, -33),
-        ["Cosmetics Shop"] = CFrame.new(-285, 3, -15),
-        ["Eggs Shop"] = CFrame.new(-284, 3, -2),
-        ["Night Event Area"] = CFrame.new(-101, 4, -7)
-    }
+	Teleports = {
+		"Seeds Shop",
+		"Sell Shop",
+		"Gear Shop",
+		"Cosmetics Shop",
+		"Eggs Shop",
+		"Night Event Area"
+	},
+	ServerEvents = {
+		["Disco"] = "DiscoEvent",
+		["Rain"] = "RainEvent",
+		["Thunderstorm"] = "Thunderstorm",
+		["Frost"] = "FrostEvent",
+		["Blood Moon"] = "BloodMoonEvent",
+		["Scheckle Rain"] = "ScheckleRain",
+		["Meteor Shower"] = "MeteorShower",
+		["Chocolate Rain"] = "ChocolateRain",
+		["Janzen Storm"] = "JanzenStorm",
+		["DJJhai"] = "DJJhai",
+		["Tornado"] = "TornadoEvent",
+		["Black Hole"] = "Blackhole",
+		["Bee Swarm"] = "BeeSwarm",
+	},
+	TeleportLocations = {
+		["Garden"] = CFrame.new(0, 0, 0),
+		["Seeds Shop"] = CFrame.new(62, 3, -27),
+		["Sell Shop"] = CFrame.new(-259, 3, -9),
+		["Gear Shop"] = CFrame.new(-285, 3, -33),
+		["Cosmetics Shop"] = CFrame.new(-285, 3, -15),
+		["Eggs Shop"] = CFrame.new(-284, 3, -2),
+		["Night Event Area"] = CFrame.new(-101, 4, -7)
+	}
 }
