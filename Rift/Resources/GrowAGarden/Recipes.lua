6ac0491ee18d157b5bb78f7d98f26e27f81ca428
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-06-15 19:51:42 +0000

    Create Recipes.lua

---
+++ Rift/Resources/GrowAGarden/Recipes.lua
@@ -0,0 +1,908 @@
+local v2 = {}
+local v3 = {}
+local v4 = {}
+local v5 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Coconut",
+        ["AcceptAllTypes"] = false
+    }
+}
+v4[1] = v5
+local v6 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Dragon Fruit",
+        ["AcceptAllTypes"] = false
+    }
+}
+v4[2] = v6
+local v7 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Mango",
+        ["AcceptAllTypes"] = false
+    }
+}
+v4[3] = v7
+local v8 = {
+    ["ItemType"] = "Sprinkler",
+    ["ItemData"] = {
+        ["ItemName"] = "Godly Sprinkler",
+        ["AcceptAllTypes"] = false
+    }
+}
+v4[4] = v8
+v3.Inputs = v4
+local v9 = {}
+local v10 = {
+    ["ItemType"] = "Gear",
+    ["ItemData"] = {
+        ["ItemName"] = "Tropical Mist Sprinkler",
+        ["Quantity"] = 1
+    }
+}
+v9[1] = v10
+v3.Outputs = v9
+v3.TimeToCraft = 3600
+v3.PurchaseID = 3306484251
+v3.RobuxPrice = 229
+v3.LayoutOrder = 10
+v3.MachineTypes = { "GearEventWorkbench" }
+v2["Tropical Mist Sprinkler"] = v3
+local v11 = {}
+local v12 = {}
+local v13 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Grape",
+        ["AcceptAllTypes"] = false
+    }
+}
+v12[1] = v13
+local v14 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Blueberry",
+        ["AcceptAllTypes"] = false
+    }
+}
+v12[2] = v14
+local v15 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Strawberry",
+        ["AcceptAllTypes"] = false
+    }
+}
+v12[3] = v15
+local v16 = {
+    ["ItemType"] = "Sprinkler",
+    ["ItemData"] = {
+        ["ItemName"] = "Godly Sprinkler",
+        ["AcceptAllTypes"] = false
+    }
+}
+v12[4] = v16
+v11.Inputs = v12
+local v17 = {}
+local v18 = {
+    ["ItemType"] = "Gear",
+    ["ItemData"] = {
+        ["ItemName"] = "Berry Blusher Sprinkler",
+        ["Quantity"] = 1
+    }
+}
+v17[1] = v18
+v11.Outputs = v17
+v11.TimeToCraft = 3600
+v11.PurchaseID = 3306484242
+v11.RobuxPrice = 229
+v11.LayoutOrder = 20
+v11.MachineTypes = { "GearEventWorkbench" }
+v2["Berry Blusher Sprinkler"] = v11
+local v19 = {}
+local v20 = {}
+local v21 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Pepper",
+        ["AcceptAllTypes"] = false
+    }
+}
+v20[1] = v21
+local v22 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Ember Lily",
+        ["AcceptAllTypes"] = false
+    }
+}
+v20[2] = v22
+local v23 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Cacao",
+        ["AcceptAllTypes"] = false
+    }
+}
+v20[3] = v23
+local v24 = {
+    ["ItemType"] = "Sprinkler",
+    ["ItemData"] = {
+        ["ItemName"] = "Master Sprinkler",
+        ["AcceptAllTypes"] = false
+    }
+}
+v20[4] = v24
+v19.Inputs = v20
+local v25 = {}
+local v26 = {
+    ["ItemType"] = "Gear",
+    ["ItemData"] = {
+        ["ItemName"] = "Spice Spritzer Sprinkler",
+        ["Quantity"] = 1
+    }
+}
+v25[1] = v26
+v19.Outputs = v25
+v19.TimeToCraft = 3600
+v19.PurchaseID = 3306484232
+v19.RobuxPrice = 229
+v19.LayoutOrder = 30
+v19.MachineTypes = { "GearEventWorkbench" }
+v2["Spice Spritzer Sprinkler"] = v19
+local v27 = {}
+local v28 = {}
+local v29 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Watermelon",
+        ["AcceptAllTypes"] = false
+    }
+}
+v28[1] = v29
+local v30 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Watermelon",
+        ["AcceptAllTypes"] = false
+    }
+}
+v28[2] = v30
+local v31 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Watermelon",
+        ["AcceptAllTypes"] = false
+    }
+}
+v28[3] = v31
+local v32 = {
+    ["ItemType"] = "Sprinkler",
+    ["ItemData"] = {
+        ["ItemName"] = "Master Sprinkler",
+        ["AcceptAllTypes"] = false
+    }
+}
+v28[4] = v32
+v27.Inputs = v28
+local v33 = {}
+local v34 = {
+    ["ItemType"] = "Gear",
+    ["ItemData"] = {
+        ["ItemName"] = "Sweet Soaker Sprinkler",
+        ["Quantity"] = 1
+    }
+}
+v33[1] = v34
+v27.Outputs = v33
+v27.TimeToCraft = 3600
+v27.PurchaseID = 3306484252
+v27.RobuxPrice = 229
+v27.LayoutOrder = 40
+v27.MachineTypes = { "GearEventWorkbench" }
+v2["Sweet Soaker Sprinkler"] = v27
+local v35 = {}
+local v36 = {}
+local v37 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Orange Tulip",
+        ["AcceptAllTypes"] = false
+    }
+}
+v36[1] = v37
+local v38 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Daffodil",
+        ["AcceptAllTypes"] = false
+    }
+}
+v36[2] = v38
+local v39 = {
+    ["ItemType"] = "Sprinkler",
+    ["ItemData"] = {
+        ["ItemName"] = "Advanced Sprinkler",
+        ["AcceptAllTypes"] = false
+    }
+}
+v36[3] = v39
+local v40 = {
+    ["ItemType"] = "Sprinkler",
+    ["ItemData"] = {
+        ["ItemName"] = "Basic Sprinkler",
+        ["AcceptAllTypes"] = false
+    }
+}
+v36[4] = v40
+v35.Inputs = v36
+local v41 = {}
+local v42 = {
+    ["ItemType"] = "Gear",
+    ["ItemData"] = {
+        ["ItemName"] = "Flower Froster Sprinkler",
+        ["Quantity"] = 1
+    }
+}
+v41[1] = v42
+v35.Outputs = v41
+v35.TimeToCraft = 3600
+v35.PurchaseID = 3306484243
+v35.RobuxPrice = 229
+v35.LayoutOrder = 50
+v35.MachineTypes = { "GearEventWorkbench" }
+v2["Flower Froster Sprinkler"] = v35
+local v43 = {}
+local v44 = {}
+local v45 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Bamboo",
+        ["AcceptAllTypes"] = false
+    }
+}
+v44[1] = v45
+local v46 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Beanstalk",
+        ["AcceptAllTypes"] = false
+    }
+}
+v44[2] = v46
+local v47 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Mushroom",
+        ["AcceptAllTypes"] = false
+    }
+}
+v44[3] = v47
+local v48 = {
+    ["ItemType"] = "Sprinkler",
+    ["ItemData"] = {
+        ["ItemName"] = "Advanced Sprinkler",
+        ["AcceptAllTypes"] = false
+    }
+}
+v44[4] = v48
+v43.Inputs = v44
+local v49 = {}
+local v50 = {
+    ["ItemType"] = "Gear",
+    ["ItemData"] = {
+        ["ItemName"] = "Stalk Sprout Sprinkler",
+        ["Quantity"] = 1
+    }
+}
+v49[1] = v50
+v43.Outputs = v49
+v43.TimeToCraft = 3600
+v43.PurchaseID = 3306484241
+v43.RobuxPrice = 229
+v43.LayoutOrder = 60
+v43.MachineTypes = { "GearEventWorkbench" }
+v2["Stalk Sprout Sprinkler"] = v43
+local v51 = {}
+local v52 = {}
+local v53 = {
+    ["ItemType"] = "SprayBottle",
+    ["ItemData"] = {
+        ["ItemName"] = "Cleaning Spray",
+        ["AcceptAllTypes"] = false
+    }
+}
+v52[1] = v53
+local v54 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Cacao",
+        ["AcceptAllTypes"] = false
+    }
+}
+v52[2] = v54
+v51.Inputs = v52
+local v55 = {}
+local v56 = {
+    ["ItemType"] = "Gear",
+    ["ItemData"] = {
+        ["ItemName"] = "Mutation Spray Choc",
+        ["Quantity"] = 1
+    }
+}
+v55[1] = v56
+v51.Outputs = v55
+v51.TimeToCraft = 720
+v51.PurchaseID = 3306484247
+v51.RobuxPrice = 179
+v51.LayoutOrder = 70
+v51.MachineTypes = { "GearEventWorkbench" }
+v51.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 20
+}
+v2["Mutation Spray Choc"] = v51
+local v57 = {}
+local v58 = {}
+local v59 = {
+    ["ItemType"] = "SprayBottle",
+    ["ItemData"] = {
+        ["ItemName"] = "Cleaning Spray",
+        ["AcceptAllTypes"] = false
+    }
+}
+v58[1] = v59
+local v60 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Bee Balm",
+        ["AcceptAllTypes"] = false
+    }
+}
+v58[2] = v60
+v57.Inputs = v58
+local v61 = {}
+local v62 = {
+    ["ItemType"] = "Gear",
+    ["ItemData"] = {
+        ["ItemName"] = "Mutation Spray Pollinated",
+        ["Quantity"] = 1
+    }
+}
+v61[1] = v62
+v57.Outputs = v61
+v57.TimeToCraft = 300
+v57.PurchaseID = 3306484250
+v57.RobuxPrice = 179
+v57.LayoutOrder = 80
+v57.MachineTypes = { "GearEventWorkbench" }
+v57.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 25
+}
+v2["Mutation Spray Pollinated"] = v57
+local v63 = {}
+local v64 = {}
+local v65 = {
+    ["ItemType"] = "SprayBottle",
+    ["ItemData"] = {
+        ["ItemName"] = "Cleaning Spray",
+        ["AcceptAllTypes"] = false
+    }
+}
+v64[1] = v65
+local v66 = {
+    ["ItemType"] = "Lightning Rod",
+    ["ItemData"] = {
+        ["ItemName"] = "Lightning Rod",
+        ["AcceptAllTypes"] = false
+    }
+}
+v64[2] = v66
+v63.Inputs = v64
+local v67 = {}
+local v68 = {
+    ["ItemType"] = "Gear",
+    ["ItemData"] = {
+        ["ItemName"] = "Mutation Spray Shocked",
+        ["Quantity"] = 1
+    }
+}
+v67[1] = v68
+v63.Outputs = v67
+v63.TimeToCraft = 1800
+v63.PurchaseID = 3306484253
+v63.RobuxPrice = 199
+v63.LayoutOrder = 90
+v63.MachineTypes = { "GearEventWorkbench" }
+v63.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 250
+}
+v2["Mutation Spray Shocked"] = v63
+local v69 = {}
+local v70 = {}
+local v71 = {
+    ["ItemType"] = "CosmeticCrate",
+    ["ItemData"] = {
+        ["ItemName"] = "Bee Crate",
+        ["AcceptAllTypes"] = false
+    }
+}
+v70[1] = v71
+v69.Inputs = v70
+local v72 = {}
+local v73 = {
+    ["ItemType"] = "CosmeticCrate",
+    ["ItemData"] = {
+        ["ItemName"] = "Honey Crafters Crate",
+        ["Quantity"] = 1
+    }
+}
+v72[1] = v73
+v69.Outputs = v72
+v69.TimeToCraft = 1800
+v69.PurchaseID = 3306484248
+v69.RobuxPrice = 179
+v69.LayoutOrder = 100
+v69.MachineTypes = { "GearEventWorkbench" }
+v69.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 12
+}
+v2["Honey Crafters Crate"] = v69
+local v74 = {}
+local v75 = {}
+local v76 = {
+    ["ItemType"] = "PetEgg",
+    ["ItemData"] = {
+        ["ItemName"] = "Bee Egg",
+        ["AcceptAllTypes"] = false
+    }
+}
+v75[1] = v76
+v74.Inputs = v75
+local v77 = {}
+local v78 = {
+    ["ItemType"] = "PetEgg",
+    ["ItemData"] = {
+        ["ItemName"] = "Anti Bee Egg",
+        ["Quantity"] = 1
+    }
+}
+v77[1] = v78
+v74.Outputs = v77
+v74.TimeToCraft = 7200
+v74.PurchaseID = 3306484244
+v74.RobuxPrice = 149
+v74.LayoutOrder = 110
+v74.MachineTypes = { "GearEventWorkbench" }
+v74.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 25
+}
+v2["Anti Bee Egg"] = v74
+local v79 = {}
+local v80 = {}
+local v81 = {
+    ["ItemType"] = "PetEgg",
+    ["ItemData"] = {
+        ["ItemName"] = "Anti Bee Egg",
+        ["AcceptAllTypes"] = false
+    }
+}
+v80[1] = v81
+local v82 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Sunflower",
+        ["AcceptAllTypes"] = false
+    }
+}
+v80[2] = v82
+local v83 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Purple Dahlia",
+        ["AcceptAllTypes"] = false
+    }
+}
+v80[3] = v83
+v79.Inputs = v80
+local v84 = {}
+local v85 = {
+    ["ItemType"] = "Pet",
+    ["ItemData"] = {
+        ["ItemName"] = "Pack Bee",
+        ["Quantity"] = 1
+    }
+}
+v84[1] = v85
+v79.Outputs = v84
+v79.TimeToCraft = 14400
+v79.PurchaseID = 3306484257
+v79.RobuxPrice = 799
+v79.LayoutOrder = 120
+v79.MachineTypes = { "GearEventWorkbench" }
+v79.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 250
+}
+v2["Pack Bee"] = v79
+local v86 = {}
+local v87 = {}
+local v88 = {
+    ["ItemType"] = "Seed Pack",
+    ["ItemData"] = {
+        ["ItemName"] = "Flower Seed Pack",
+        ["AcceptAllTypes"] = false
+    }
+}
+v87[1] = v88
+v86.Inputs = v87
+local v89 = {}
+local v90 = {
+    ["ItemType"] = "Seed Pack",
+    ["ItemData"] = {
+        ["ItemName"] = "Crafters Seed Pack",
+        ["Quantity"] = 1
+    }
+}
+v89[1] = v90
+v86.Outputs = v89
+v86.TimeToCraft = 1200
+v86.PurchaseID = 3306485034
+v86.RobuxPrice = 199
+v86.LayoutOrder = 130
+v86.MachineTypes = { "SeedEventWorkbench" }
+v86.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 10
+}
+v2["Crafters Seed Pack"] = v86
+local v91 = {}
+local v92 = {}
+local v93 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Daffodil",
+        ["AcceptAllTypes"] = false
+    }
+}
+v92[1] = v93
+local v94 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Orange Tulip",
+        ["AcceptAllTypes"] = false
+    }
+}
+v92[2] = v94
+v91.Inputs = v92
+local v95 = {}
+local v96 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Manuka Flower",
+        ["Quantity"] = 1
+    }
+}
+v95[1] = v96
+v91.Outputs = v95
+v91.TimeToCraft = 600
+v91.PurchaseID = 3306485030
+v91.RobuxPrice = 99
+v91.LayoutOrder = 140
+v91.MachineTypes = { "SeedEventWorkbench" }
+v91.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 6
+}
+v2["Manuka Flower"] = v91
+local v97 = {}
+local v98 = {}
+local v99 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Bamboo",
+        ["AcceptAllTypes"] = false
+    }
+}
+v98[1] = v99
+local v100 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Bamboo",
+        ["AcceptAllTypes"] = false
+    }
+}
+v98[2] = v100
+local v101 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Manuka Flower",
+        ["AcceptAllTypes"] = false
+    }
+}
+v98[3] = v101
+v97.Inputs = v98
+local v102 = {}
+local v103 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Dandelion",
+        ["Quantity"] = 1
+    }
+}
+v102[1] = v103
+v97.Outputs = v102
+v97.TimeToCraft = 960
+v97.PurchaseID = 3306485029
+v97.RobuxPrice = 199
+v97.LayoutOrder = 150
+v97.MachineTypes = { "SeedEventWorkbench" }
+v97.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 20
+}
+v2.Dandelion = v97
+local v104 = {}
+local v105 = {}
+local v106 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Pumpkin",
+        ["AcceptAllTypes"] = false
+    }
+}
+v105[1] = v106
+local v107 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Pumpkin",
+        ["AcceptAllTypes"] = false
+    }
+}
+v105[2] = v107
+local v108 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Dandelion",
+        ["AcceptAllTypes"] = false
+    }
+}
+v105[3] = v108
+local v109 = {
+    ["ItemType"] = "Seed Pack",
+    ["ItemData"] = {
+        ["ItemName"] = "Flower Seed Pack",
+        ["AcceptAllTypes"] = false
+    }
+}
+v105[4] = v109
+v104.Inputs = v105
+local v110 = {}
+local v111 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Lumira",
+        ["Quantity"] = 1
+    }
+}
+v110[1] = v111
+v104.Outputs = v110
+v104.TimeToCraft = 1200
+v104.PurchaseID = 3306485023
+v104.RobuxPrice = 529
+v104.LayoutOrder = 160
+v104.MachineTypes = { "SeedEventWorkbench" }
+v104.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 40
+}
+v2.Lumira = v104
+local v112 = {}
+local v113 = {}
+local v114 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Pink Lily",
+        ["AcceptAllTypes"] = false
+    }
+}
+v113[1] = v114
+local v115 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Purple Dahlia",
+        ["AcceptAllTypes"] = false
+    }
+}
+v113[2] = v115
+v112.Inputs = v113
+local v116 = {}
+local v117 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Honeysuckle",
+        ["Quantity"] = 1
+    }
+}
+v116[1] = v117
+v112.Outputs = v116
+v112.TimeToCraft = 1440
+v112.PurchaseID = 3306485032
+v112.RobuxPrice = 619
+v112.LayoutOrder = 170
+v112.MachineTypes = { "SeedEventWorkbench" }
+v112.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 80
+}
+v2.Honeysuckle = v112
+local v118 = {}
+local v119 = {}
+local v120 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Crocus",
+        ["AcceptAllTypes"] = false
+    }
+}
+v119[1] = v120
+local v121 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Lavender",
+        ["AcceptAllTypes"] = false
+    }
+}
+v119[2] = v121
+v118.Inputs = v119
+local v122 = {}
+local v123 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Bee Balm",
+        ["Quantity"] = 1
+    }
+}
+v122[1] = v123
+v118.Outputs = v122
+v118.TimeToCraft = 900
+v118.PurchaseID = 3306485036
+v118.RobuxPrice = 179
+v118.LayoutOrder = 180
+v118.MachineTypes = { "SeedEventWorkbench" }
+v118.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 10
+}
+v2["Bee Balm"] = v118
+local v124 = {}
+local v125 = {}
+local v126 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Cactus",
+        ["AcceptAllTypes"] = false
+    }
+}
+v125[1] = v126
+local v127 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Cactus",
+        ["AcceptAllTypes"] = false
+    }
+}
+v125[2] = v127
+local v128 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Cactus",
+        ["AcceptAllTypes"] = false
+    }
+}
+v125[3] = v128
+local v129 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Nectarshade",
+        ["AcceptAllTypes"] = false
+    }
+}
+v125[4] = v129
+v124.Inputs = v125
+local v130 = {}
+local v131 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Nectar Thorn",
+        ["Quantity"] = 1
+    }
+}
+v130[1] = v131
+v124.Outputs = v130
+v124.TimeToCraft = 1800
+v124.PurchaseID = 3306485033
+v124.RobuxPrice = 659
+v124.LayoutOrder = 190
+v124.MachineTypes = { "SeedEventWorkbench" }
+v124.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 20
+}
+v2["Nectar Thorn"] = v124
+local v132 = {}
+local v133 = {}
+local v134 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Crocus",
+        ["AcceptAllTypes"] = false
+    }
+}
+v133[1] = v134
+local v135 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Daffodil",
+        ["AcceptAllTypes"] = false
+    }
+}
+v133[2] = v135
+local v136 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Dandelion",
+        ["AcceptAllTypes"] = false
+    }
+}
+v133[3] = v136
+local v137 = {
+    ["ItemType"] = "Holdable",
+    ["ItemData"] = {
+        ["ItemName"] = "Pink Lily",
+        ["AcceptAllTypes"] = false
+    }
+}
+v133[4] = v137
+v132.Inputs = v133
+local v138 = {}
+local v139 = {
+    ["ItemType"] = "Seed",
+    ["ItemData"] = {
+        ["ItemName"] = "Suncoil",
+        ["Quantity"] = 1
+    }
+}
+v138[1] = v139
+v132.Outputs = v138
+v132.TimeToCraft = 2700
+v132.PurchaseID = 3306485035
+v132.RobuxPrice = 749
+v132.LayoutOrder = 200
+v132.MachineTypes = { "SeedEventWorkbench" }
+v132.Cost = {
+    ["CurrencyType"] = "Honey",
+    ["Amount"] = 40
+}
+v2.Suncoil = v132
+local v140 = {}
+for v141, v142 in v2 do
+    for _, v143 in v142.MachineTypes do
+        local v144 = v140[v143]
+        if not v144 then
+            v144 = {}
+            v140[v143] = v144
+        end
+        v144[v141] = v142
+    end
+end
+return {
+    ["ItemRecipes"] = v2,
+    ["RecipiesSortedByMachineType"] = v140
+}
