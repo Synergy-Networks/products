51a183688b495e0a70fec8d9ec5cb93952a5a3ef
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-06-28 15:53:43 +0000

    Update Gears.lua

--- Rift/Resources/GrowAGarden/Gears.lua
+++ Rift/Resources/GrowAGarden/Gears.lua
@@ -1,4 +1,3 @@
-local v1 = game:GetService("ReplicatedStorage")
 local v3 = {
 	["Watering Can"] = {
 		["GearName"] = "Watering Can",
@@ -13,8 +12,9 @@ local v3 = {
 		["GiftPurchaseID"] = 3269349334,
 		["Stack"] = 10,
 		["DisplayInShop"] = true,
-		["LayoutOrder"] = 1,
+		["LayoutOrder"] = 10,
 		["Asset"] = "rbxassetid://140223014467344",
+		["FallbackPrice"] = 39
 	},
 	["Trowel"] = {
 		["GearName"] = "Trowel",
@@ -28,8 +28,9 @@ local v3 = {
 		["PurchaseID"] = 3265946561,
 		["Stack"] = 5,
 		["DisplayInShop"] = true,
-		["LayoutOrder"] = 2,
+		["LayoutOrder"] = 20,
 		["Asset"] = "rbxassetid://115261280019001",
+		["FallbackPrice"] = 49
 	},
 	["Recall Wrench"] = {
 		["GearName"] = "Recall Wrench",
@@ -44,8 +45,9 @@ local v3 = {
 		["GiftPurchaseID"] = 3282918955,
 		["Stack"] = 5,
 		["DisplayInShop"] = true,
-		["LayoutOrder"] = 3,
+		["LayoutOrder"] = 30,
 		["Asset"] = "rbxassetid://98327818593168",
+		["FallbackPrice"] = 59
 	},
 	["Basic Sprinkler"] = {
 		["GearName"] = "Basic Sprinkler",
@@ -60,8 +62,9 @@ local v3 = {
 		["GiftPurchaseID"] = 3269349445,
 		["Stack"] = 1,
 		["DisplayInShop"] = true,
-		["LayoutOrder"] = 4,
+		["LayoutOrder"] = 40,
 		["Asset"] = "rbxassetid://120314005864914",
+		["FallbackPrice"] = 79
 	},
 	["Advanced Sprinkler"] = {
 		["GearName"] = "Advanced Sprinkler",
@@ -76,8 +79,9 @@ local v3 = {
 		["GiftPurchaseID"] = 3269349768,
 		["Stack"] = 1,
 		["DisplayInShop"] = true,
-		["LayoutOrder"] = 5,
+		["LayoutOrder"] = 50,
 		["Asset"] = "rbxassetid://90193997839883",
+		["FallbackPrice"] = 99
 	},
 	["Godly Sprinkler"] = {
 		["GearName"] = "Godly Sprinkler",
@@ -92,168 +96,44 @@ local v3 = {
 		["GiftPurchaseID"] = 3269349908,
 		["Stack"] = 1,
 		["DisplayInShop"] = true,
-		["LayoutOrder"] = 6,
+		["LayoutOrder"] = 60,
 		["Asset"] = "rbxassetid://75442797836082",
+		["FallbackPrice"] = 149
 	},
 	["Lightning Rod"] = {
 		["GearName"] = "Lightning Rod",
 		["GearRarity"] = "Mythical",
-		["StockChance"] = 50,
+		["StockChance"] = 0,
 		["StockAmount"] = {
-			1,
-			1
+			0,
+			0
 		},
-		["Price"] = 1000000,
+		["Price"] = 1000000000000,
 		["PurchaseID"] = 3265946758,
 		["GiftPurchaseID"] = 3274108730,
 		["Stack"] = 1,
-		["DisplayInShop"] = true,
-		["LayoutOrder"] = 7,
+		["DisplayInShop"] = false,
+		["LayoutOrder"] = 70,
 		["Asset"] = "rbxassetid://125433095334047",
+		["FallbackPrice"] = 179
 	},
-	["Master Sprinkler"] = {
-		["GearName"] = "Master Sprinkler",
-		["GearRarity"] = "Divine",
-		["StockChance"] = 100,
+	["Magnifying Glass"] = {
+		["GearName"] = "Magnifying Glass",
+		["GearRarity"] = "Mythical",
+		["StockChance"] = 3,
 		["StockAmount"] = {
 			1,
 			1
 		},
 		["Price"] = 10000000,
-		["PurchaseID"] = 3267580365,
-		["GiftPurchaseID"] = 3269350167,
-		["Stack"] = 1,
-		["DisplayInShop"] = true,
-		["LayoutOrder"] = 8,
-		["Asset"] = "rbxassetid://98504519094449",
-	},
-	["Cleaning Spray"] = {
-		["GearName"] = "Cleaning Spray",
-		["GearRarity"] = "Divine",
-		["StockChance"] = 1,
-		["StockAmount"] = {
-			1,
-			5
-		},
-		["Price"] = 15000000,
-		["PurchaseID"] = 3306767043,
-		["GiftPurchaseID"] = 3306767451,
+		["PurchaseID"] = 3316261725,
+		["GiftPurchaseID"] = 3317955209,
 		["Stack"] = 10,
-		["LayoutOrder"] = 9,
 		["DisplayInShop"] = true,
-		["Asset"] = "rbxassetid://134458369683392",
-	},
-	["Favorite Tool"] = {
-		["GearName"] = "Favorite Tool",
-		["GearRarity"] = "Divine",
-		["StockChance"] = 1,
-		["StockAmount"] = {
-			1,
-			3
-		},
-		["Price"] = 20000000,
-		["PurchaseID"] = 3281679093,
-		["Stack"] = 20,
-		["GiftPurchaseID"] = 3281679185,
-		["DisplayInShop"] = true,
-		["LayoutOrder"] = 10,
-		["Asset"] = "rbxassetid://129676033321965",
-	},
-	["Harvest Tool"] = {
-		["GearName"] = "Harvest Tool",
-		["GearRarity"] = "Divine",
-		["StockChance"] = 1,
-		["StockAmount"] = {
-			1,
-			3
-		},
-		["Price"] = 30000000,
-		["PurchaseID"] = 3286038236,
-		["Stack"] = 5,
-		["GiftPurchaseID"] = 3286038078,
-		["LayoutOrder"] = 11,
-		["DisplayInShop"] = true,
-		["Asset"] = "rbxassetid://99521037861537",
-	},
-	["Friendship Pot"] = {
-		["GearName"] = "Friendship Pot",
-		["GearRarity"] = "Divine",
-		["StockChance"] = 50,
-		["StockAmount"] = {
-			1,
-			1
-		},
-		["Price"] = 15000000,
-		["PurchaseID"] = 3301473650,
-		["Stack"] = 1,
-		["GiftPurchaseID"] = 3301474767,
-		["LayoutOrder"] = 12,
-		["DisplayInShop"] = true,
-		["Asset"] = "rbxassetid://106888620308041",
-	},
-	["Mutation Spray Choc"] = {
-		["GearName"] = "Mutation Spray Choc",
-		["GearRarity"] = "Divine",
-		["StockChance"] = 0,
-		["StockAmount"] = {
-			0,
-			0
-		},
-		["Price"] = 10000000000000,
-		["PurchaseID"] = 0,
-		["Stack"] = 10,
-		["GiftPurchaseID"] = 0,
-		["LayoutOrder"] = 13,
-		["DisplayInShop"] = false,
-		["Asset"] = "rbxassetid://104900539098874",
-	},
-	["Mutation Spray Pollinated"] = {
-		["GearName"] = "Mutation Spray Choc",
-		["GearRarity"] = "Divine",
-		["StockChance"] = 0,
-		["StockAmount"] = {
-			0,
-			0
-		},
-		["Price"] = 10000000000000,
-		["PurchaseID"] = 0,
-		["Stack"] = 10,
-		["GiftPurchaseID"] = 0,
-		["LayoutOrder"] = 13,
-		["DisplayInShop"] = false,
-		["Asset"] = "rbxassetid://103170047234524",
-	},
-	["Mutation Spray Shocked"] = {
-		["GearName"] = "Mutation Spray Shocked",
-		["GearRarity"] = "Divine",
-		["StockChance"] = 0,
-		["StockAmount"] = {
-			0,
-			0
-		},
-		["Price"] = 10000000000000,
-		["PurchaseID"] = 0,
-		["Stack"] = 10,
-		["GiftPurchaseID"] = 0,
-		["LayoutOrder"] = 14,
-		["DisplayInShop"] = false,
-		["Asset"] = "rbxassetid://131806798815295",
-	},
-	["Reclaimer"] = {
-		["GearName"] = "Reclaimer",
-		["GearRarity"] = "Divine",
-		["StockChance"] = 1,
-		["StockAmount"] = {
-			5,
-			10
-		},
-		["Price"] = 10000000000000,
-		["PurchaseID"] = 0,
-		["Stack"] = 1,
-		["GiftPurchaseID"] = 3309685958,
-		["LayoutOrder"] = 15,
-		["DisplayInShop"] = false,
-		["Asset"] = "rbxassetid://79034654924763",
+		["LayoutOrder"] = 80,
+		["Asset"] = "rbxassetid://107914377975445",
+		["GearDescription"] = v2.color(Color3.fromRGB(85, 255, 255))("Inspect plants to reveal the value, without collecting them!"),
+		["FallbackPrice"] = 99
 	},
 	["Tanning Mirror"] = {
 		["GearName"] = "Tanning Mirror",
@@ -268,8 +148,219 @@ local v3 = {
 		["GiftPurchaseID"] = 3311160347,
 		["Stack"] = 1,
 		["DisplayInShop"] = true,
-		["LayoutOrder"] = 7,
+		["LayoutOrder"] = 90,
 		["Asset"] = "rbxassetid://81619835086288",
+		["GearDescription"] = v2.color(Color3.fromRGB(255, 255, 0))("Redirects Sun Beams 10 times before being destroyed"),
+		["FallbackPrice"] = 219
+	},
+	["Master Sprinkler"] = {
+		["GearName"] = "Master Sprinkler",
+		["GearRarity"] = "Divine",
+		["StockChance"] = 100,
+		["StockAmount"] = {
+			1,
+			1
+		},
+		["Price"] = 10000000,
+		["PurchaseID"] = 3267580365,
+		["GiftPurchaseID"] = 3269350167,
+		["Stack"] = 1,
+		["DisplayInShop"] = true,
+		["LayoutOrder"] = 100,
+		["Asset"] = "rbxassetid://98504519094449",
+		["GearDescription"] = v2.yellow("Greatly increases growth speed, mutation chances and fruit size. Lasts 10 minutes"),
+		["FallbackPrice"] = 199
+	},
+	["Cleaning Spray"] = {
+		["GearName"] = "Cleaning Spray",
+		["GearRarity"] = "Divine",
+		["StockChance"] = 1,
+		["StockAmount"] = {
+			1,
+			3
+		},
+		["Price"] = 15000000,
+		["PurchaseID"] = 3306767043,
+		["GiftPurchaseID"] = 3306767451,
+		["Stack"] = 10,
+		["DisplayInShop"] = true,
+		["LayoutOrder"] = 110,
+		["Asset"] = "rbxassetid://134458369683392",
+		["GearDescription"] = v2.white("Cleans mutations off fruit! 10 Uses"),
+		["FallbackPrice"] = 139
+	},
+	["Favorite Tool"] = {
+		["GearName"] = "Favorite Tool",
+		["GearRarity"] = "Divine",
+		["StockChance"] = 1,
+		["StockAmount"] = {
+			1,
+			3
+		},
+		["Price"] = 20000000,
+		["PurchaseID"] = 3281679093,
+		["Stack"] = 20,
+		["GiftPurchaseID"] = 3281679185,
+		["DisplayInShop"] = true,
+		["LayoutOrder"] = 120,
+		["Asset"] = "rbxassetid://129676033321965",
+		["GearDescription"] = v2.orange("Favorites your fruit to prevent collecting.") .. v2.orange(" 20 uses"),
+		["FallbackPrice"] = 119
+	},
+	["Harvest Tool"] = {
+		["GearName"] = "Harvest Tool",
+		["GearRarity"] = "Divine",
+		["StockChance"] = 1,
+		["StockAmount"] = {
+			1,
+			3
+		},
+		["Price"] = 30000000,
+		["PurchaseID"] = 3286038236,
+		["Stack"] = 5,
+		["GiftPurchaseID"] = 3286038078,
+		["DisplayInShop"] = true,
+		["Asset"] = "rbxassetid://99521037861537",
+		["LayoutOrder"] = 130,
+		["GearDescription"] = v2.orange("Harvest all fruit from a chosen plant. 5 uses"),
+		["FallbackPrice"] = 149
+	},
+	["Friendship Pot"] = {
+		["GearName"] = "Friendship Pot",
+		["GearRarity"] = "Divine",
+		["StockChance"] = 50,
+		["StockAmount"] = {
+			1,
+			1
+		},
+		["Price"] = 15000000,
+		["PurchaseID"] = 3301473650,
+		["Stack"] = 1,
+		["GiftPurchaseID"] = 3301474767,
+		["DisplayInShop"] = true,
+		["Asset"] = "rbxassetid://106888620308041",
+		["LayoutOrder"] = 140,
+		["GearDescription"] = v2.pink("A flower pot to share with a friend!"),
+		["FallbackPrice"] = 39
+	},
+	["Mutation Spray Choc"] = {
+		["GearName"] = "Mutation Spray Choc",
+		["GearRarity"] = "Divine",
+		["StockChance"] = 0,
+		["StockAmount"] = {
+			0,
+			0
+		},
+		["Price"] = 10000000000000,
+		["PurchaseID"] = 0,
+		["Stack"] = 10,
+		["GiftPurchaseID"] = 0,
+		["DisplayInShop"] = false,
+		["Asset"] = "rbxassetid://104900539098874",
+		["LayoutOrder"] = 150,
+		["GearDescription"] = v2.color(Color3.fromRGB(148, 103, 82))("Covers a fruit in Choc!")
+	},
+	["Mutation Spray Pollinated"] = {
+		["GearName"] = "Mutation Spray Choc",
+		["GearRarity"] = "Divine",
+		["StockChance"] = 0,
+		["StockAmount"] = {
+			0,
+			0
+		},
+		["Price"] = 10000000000000,
+		["PurchaseID"] = 0,
+		["Stack"] = 10,
+		["GiftPurchaseID"] = 0,
+		["DisplayInShop"] = false,
+		["Asset"] = "rbxassetid://103170047234524",
+		["LayoutOrder"] = 160,
+		["GearDescription"] = v2.color(Color3.fromRGB(255, 170, 0))("Pollinates a fruit!")
+	},
+	["Mutation Spray Shocked"] = {
+		["GearName"] = "Mutation Spray Shocked",
+		["GearRarity"] = "Divine",
+		["StockChance"] = 0,
+		["StockAmount"] = {
+			0,
+			0
+		},
+		["Price"] = 10000000000000,
+		["PurchaseID"] = 0,
+		["Stack"] = 10,
+		["GiftPurchaseID"] = 0,
+		["DisplayInShop"] = false,
+		["Asset"] = "rbxassetid://131806798815295",
+		["LayoutOrder"] = 170,
+		["GearDescription"] = v2.color(Color3.fromRGB(255, 255, 100))("Shocks a fruit!")
+	},
+	["Mutation Spray Chilled"] = {
+		["GearName"] = "Mutation Spray Chilled",
+		["GearRarity"] = "Divine",
+		["StockChance"] = 0,
+		["StockAmount"] = {
+			0,
+			0
+		},
+		["Price"] = 10000000000000,
+		["PurchaseID"] = 0,
+		["Stack"] = 10,
+		["GiftPurchaseID"] = 0,
+		["DisplayInShop"] = false,
+		["Asset"] = "rbxassetid://81133409755490",
+		["LayoutOrder"] = 170,
+		["GearDescription"] = v2.color(Color3.fromRGB(255, 255, 100))("Chills a fruit!")
+	},
+	["Mutation Spray Burnt"] = {
+		["GearName"] = "Mutation Spray Burnt",
+		["GearRarity"] = "Divine",
+		["StockChance"] = 0,
+		["StockAmount"] = {
+			0,
+			0
+		},
+		["Price"] = 10000000000000,
+		["PurchaseID"] = 0,
+		["Stack"] = 10,
+		["GiftPurchaseID"] = 0,
+		["DisplayInShop"] = false,
+		["Asset"] = "rbxassetid://99446610437201",
+		["LayoutOrder"] = 170,
+		["GearDescription"] = v2.color(Color3.fromRGB(255, 255, 100))("Burns a fruit!")
+	},
+	["Mutation Spray Cloudtouched"] = {
+		["GearName"] = "Mutation Spray Cloudtouched",
+		["GearRarity"] = "Divine",
+		["StockChance"] = 0,
+		["StockAmount"] = {
+			0,
+			0
+		},
+		["Price"] = 10000000000000,
+		["PurchaseID"] = 0,
+		["Stack"] = 10,
+		["GiftPurchaseID"] = 0,
+		["DisplayInShop"] = false,
+		["Asset"] = "rbxassetid://83361659792737",
+		["LayoutOrder"] = 170,
+		["GearDescription"] = v2.color(Color3.fromRGB(255, 255, 100))("Fruits become blessed by clouds!")
+	},
+	["Reclaimer"] = {
+		["GearName"] = "Reclaimer",
+		["GearRarity"] = "Divine",
+		["StockChance"] = 1,
+		["StockAmount"] = {
+			5,
+			10
+		},
+		["Price"] = 10000000000000,
+		["PurchaseID"] = 0,
+		["Stack"] = 1,
+		["GiftPurchaseID"] = 3309685958,
+		["DisplayInShop"] = false,
+		["Asset"] = "rbxassetid://79034654924763",
+		["LayoutOrder"] = 180,
+		["GearDescription"] = v2.color(Color3.fromRGB(0, 170, 0))("Reclaims plants, turning them into seeds!")
 	},
 	["Tropical Mist Sprinkler"] = {
 		["GearName"] = "Tropical Mist Sprinkler",
@@ -284,7 +375,7 @@ local v3 = {
 		["GiftPurchaseID"] = 0,
 		["Stack"] = 1,
 		["DisplayInShop"] = false,
-		["LayoutOrder"] = 0,
+		["LayoutOrder"] = 200,
 		["Asset"] = "rbxassetid://122592465315585",
 	},
 	["Spice Spritzer Sprinkler"] = {
@@ -300,7 +391,7 @@ local v3 = {
 		["GiftPurchaseID"] = 0,
 		["Stack"] = 1,
 		["DisplayInShop"] = false,
-		["LayoutOrder"] = 0,
+		["LayoutOrder"] = 210,
 		["Asset"] = "rbxassetid://130541096369176",
 	},
 	["Sweet Soaker Sprinkler"] = {
@@ -316,7 +407,7 @@ local v3 = {
 		["GiftPurchaseID"] = 0,
 		["Stack"] = 1,
 		["DisplayInShop"] = false,
-		["LayoutOrder"] = 0,
+		["LayoutOrder"] = 220,
 		["Asset"] = "rbxassetid://91036428046936",
 	},
 	["Stalk Sprout Sprinkler"] = {
@@ -332,7 +423,7 @@ local v3 = {
 		["GiftPurchaseID"] = 0,
 		["Stack"] = 1,
 		["DisplayInShop"] = false,
-		["LayoutOrder"] = 0,
+		["LayoutOrder"] = 230,
 		["Asset"] = "rbxassetid://125326661954614",
 	},
 	["Berry Blusher Sprinkler"] = {
@@ -348,7 +439,7 @@ local v3 = {
 		["GiftPurchaseID"] = 0,
 		["Stack"] = 1,
 		["DisplayInShop"] = false,
-		["LayoutOrder"] = 0,
+		["LayoutOrder"] = 240,
 		["Asset"] = "rbxassetid://121776369197466",
 	},
 	["Flower Froster Sprinkler"] = {
@@ -364,7 +455,7 @@ local v3 = {
 		["GiftPurchaseID"] = 0,
 		["Stack"] = 1,
 		["DisplayInShop"] = false,
-		["LayoutOrder"] = 0,
+		["LayoutOrder"] = 250,
 		["Asset"] = "rbxassetid://86602672756084",
 	}
 }
