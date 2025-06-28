1a86bf7ba5c5b6b735a58b4d542e3b53f972b392
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-06-28 15:56:26 +0000

    Update Eggs.lua

--- Rift/Resources/GrowAGarden/Eggs.lua
+++ Rift/Resources/GrowAGarden/Eggs.lua
@@ -1,4 +1,3 @@
-local v1 = game:GetService("ReplicatedStorage")
 local v2 = {
 	["Common Egg"] = {
 		["EggName"] = "Common Egg",
@@ -9,39 +8,21 @@ local v2 = {
 		["LayoutOrder"] = 1,
 		["PetEggDescription"] = "It\'s a dog"
 	},
-	["Uncommon Egg"] = {
-		["EggName"] = "Uncommon Egg",
-		["EggRarity"] = "Uncommon",
-		["StockChance"] = 6,
-		["Price"] = 150000,
-		["PurchaseID"] = 3276346509,
-		["LayoutOrder"] = 1,
-		["PetEggDescription"] = "It\'s a dog"
-	},
-	["Rare Egg"] = {
-		["EggName"] = "Rare Egg",
-		["EggRarity"] = "Rare",
-		["StockChance"] = 20,
-		["Price"] = 600000,
-		["PurchaseID"] = 3276346557,
-		["LayoutOrder"] = 1,
-		["PetEggDescription"] = "It\'s a dog"
-	},
-	["Legendary Egg"] = {
-		["EggName"] = "Legendary Egg",
-		["EggRarity"] = "Legendary",
-		["StockChance"] = 50,
-		["Price"] = 3000000,
-		["PurchaseID"] = 3276346676,
-		["LayoutOrder"] = 1,
-		["PetEggDescription"] = "It\'s a dog"
-	},
 	["Mythical Egg"] = {
 		["EggName"] = "Mythical Egg",
 		["EggRarity"] = "Mythical",
 		["StockChance"] = 70,
 		["Price"] = 8000000,
 		["PurchaseID"] = 3286560171,
+		["LayoutOrder"] = 1,
+		["PetEggDescription"] = "It\'s a dog"
+	},
+	["Bee Egg"] = {
+		["EggName"] = "Bee Egg",
+		["EggRarity"] = "Mythical",
+		["StockChance"] = 85,
+		["Price"] = 30000000,
+		["PurchaseID"] = 3295398638,
 		["LayoutOrder"] = 1,
 		["PetEggDescription"] = "It\'s a dog"
 	},
