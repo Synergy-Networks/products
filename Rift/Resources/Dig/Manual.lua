467d87f9c277be466d3f1bcd0d5779e21e02ec2b
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-06-28 22:20:58 +0000

    Create Manual.lua

---
+++ Rift/Resources/Dig/Manual.lua
@@ -0,0 +1,15 @@
+return {
+	Magnets = {
+		"Red Magnet",
+		"Light Bulb",
+		"Odd Mushroom",
+		"Green Magnet",
+		"Fortuned Magnet",
+		"Ghost Magnet",
+		"Legendary Magnet",
+		"Golden Horseshoe",
+		"Fossil Brush",
+		"Prismatic Magnet",
+		"wisdom"
+	}
+}
