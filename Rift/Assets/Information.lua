2b9a37e2b0780856863b47bd23aac4ea7feef4c7
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-06-29 14:40:10 +0000

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -1,5 +1,5 @@
 return {
-	VERSION = "1.25.11.1",
+	VERSION = "1.25.12",
 	CHANNEL = "Beta",
 	ADVERTISEMENT_ENABLED = true,
 	CHANGELOGS = "View the change logs at https://docs.rifton.top/changelogs.",
@@ -7,6 +7,7 @@ return {
 			"<font color=\"#ffff00\">◉</font> → Experimental / Has Issues\n" ..
 			"<font color=\"#ff0000\">◉</font> → Offline / Broken\n\n" ..
 			"<font color=\"#00ff00\">◉</font> <b>Grow a Garden</b> <font color=\"#00ff00\">[NEW]</font>\n" ..
+			"<font color=\"#ffff00\">◉</font> <b>Dig</b>" ..
 			"<font color=\"#00ff00\">◉</font> <b>Dead Rails</b>\n" ..
 			"<font color=\"#00ff00\">◉</font> <b>Forsaken</b>",
 	SCRIPT_NAMES = {
