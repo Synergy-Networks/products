1d2d5cc2099586f5d94a60b6c08537adaeed3876
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-07-01 16:09:33 +0000

    Create Manual.lua

---
+++ Rift/Resources/99NightsInTheForest/Manual.lua
@@ -0,0 +1,17 @@
+return {
+  ItemNames = {
+  	"Log Chair",
+  	"Coal",
+  	"Fuel Canister",
+  	"Oil Barrel",
+  	"Biofuel",
+  	"Carrot",
+  	"Berry",
+  	"Morsel",
+  	"Steak",
+  	"Cooked Morsel",
+  	"Cooked Steak",
+  	"Bandage",
+  	"MedKit"
+  }
+}
