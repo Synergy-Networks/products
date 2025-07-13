62ea1211cacd0da591157bace5160be2ee61a350
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-07-13 18:00:51 +0000

    Update loader.lua

--- BetterBypasser/loader.lua
+++ BetterBypasser/loader.lua
@@ -1,1 +1,1 @@
-return game.Players.LocalPlayer:Kick("This script is now discontinued. discord.gg/synergynetworks")
+return game.Players.LocalPlayer:Kick("This script is now discontinued. Go to discord.gg/synergynetworks to get the AI chatbot feature if you want")
