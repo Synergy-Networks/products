df28814d493057dd18910ac7ebdfa292e281b28b
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-07-14 19:48:50 +0000

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -1,5 +1,5 @@
 return {
-	VERSION = "1.25.17.4",
+	VERSION = "1.25.18",
 	CHANNEL = "Beta",
 	ADVERTISEMENT_ENABLED = false,
 	CHANGELOGS = "View the change logs at https://docs.rifton.top/changelogs.",
@@ -7,20 +7,16 @@ return {
 			"<font color=\"#ffff00\">◉</font> → Experimental / Has Issues\n" ..
 			"<font color=\"#ff0000\">◉</font> → Offline / Broken\n\n" ..
 			"<font color=\"#00ff00\">◉</font> <b>Grow a Garden</b>\n" ..
+			"<font color=\"#00ff00\">◉</font> <b>Steal a Brainrot</b>\n" ..
+			"<font color=\"#00ff00\">◉</font> <b>Dead Rails</b>\n" ..
 			"<font color=\"#00ff00\">◉</font> <b>99 Nights in the Forest</b> <font color=\"#00ff00\">[NEW]</font>\n" ..
 			"<font color=\"#00ff00\">◉</font> <b>Ink Game</b> <font color=\"#00ff00\">[NEW]</font>\n" ..
-			"<font color=\"#00ff00\">◉</font> <b>Dead Rails</b>\n" ..
 			"<font color=\"#ff0000\">◉</font> <b>Dig</b> <font color=\"#00ff00\">[NEW]</font>\n" ..
 			"<font color=\"#00ff00\">◉</font> <b>Forsaken</b>",
 	SCRIPT_NAMES = {
-		"BetterBypasser (Chat Bypass)",
 		"Rizzler"
 	},
 	SCRIPT_LINKS = {
-		["BetterBypasser (Chat Bypass)"] = [[-- Discord Server: https://vaultcord.win/synergy
--- Join our server to get consistent updates about our products and services
-
-loadstring(game:HttpGet("https://github.com/Synergy-Networks/products/raw/main/BetterBypasser/loader.lua"))()]],
 		["Rizzler"] = [[-- Discord Server: https://vaultcord.win/synergy
 -- Join our server to get consistent updates about our products and services
 
