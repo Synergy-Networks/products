99882a5bb48a822eae6738179587f37311c8f492
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-08-25 19:39:27 +0000

    Update Eggs.lua

--- Rift/Resources/GrowAGarden/Eggs.lua
+++ Rift/Resources/GrowAGarden/Eggs.lua
@@ -1,66 +1,103 @@
+
 local v2 = {
-	["Common Egg"] = {
-		["EggName"] = "Common Egg",
-		["EggRarity"] = "Common",
-		["StockChance"] = 1,
-		["Price"] = 50000,
-		["PurchaseID"] = 3276346455,
-		["LayoutOrder"] = 1,
-		["PetEggDescription"] = "It\'s a dog"
-	},
-	["Mythical Egg"] = {
-		["EggName"] = "Mythical Egg",
-		["EggRarity"] = "Mythical",
-		["StockChance"] = 70,
-		["Price"] = 8000000,
-		["PurchaseID"] = 3286560171,
-		["LayoutOrder"] = 1,
-		["PetEggDescription"] = "It\'s a dog"
-	},
-	["Bee Egg"] = {
-		["EggName"] = "Bee Egg",
-		["EggRarity"] = "Mythical",
-		["StockChance"] = 85,
-		["Price"] = 30000000,
-		["PurchaseID"] = 3295398638,
-		["LayoutOrder"] = 1,
-		["PetEggDescription"] = "It\'s a dog"
-	},
-	["Bug Egg"] = {
-		["EggName"] = "Bug Egg",
-		["EggRarity"] = "Divine",
-		["StockChance"] = 100,
-		["Price"] = 50000000,
-		["PurchaseID"] = 3277000452,
-		["LayoutOrder"] = 1,
-		["PetEggDescription"] = "It\'s a dog"
-	},
-	["Common Summer Egg"] = {
-		["EggName"] = "Common Summer Egg",
-		["EggRarity"] = "Common",
-		["StockChance"] = 12,
-		["Price"] = 1000000,
-		["PurchaseID"] = 3312016380,
-		["LayoutOrder"] = 1,
-		["PetEggDescription"] = "It\'s a dog"
-	},
-	["Rare Summer Egg"] = {
-		["EggName"] = "Rare Summer Egg",
-		["EggRarity"] = "Rare",
-		["StockChance"] = 40,
-		["Price"] = 25000000,
-		["PurchaseID"] = 3312016506,
-		["LayoutOrder"] = 1,
-		["PetEggDescription"] = "It\'s a dog"
-	},
-	["Paradise Egg"] = {
-		["EggName"] = "Paradise Egg",
-		["EggRarity"] = "Mythical",
-		["StockChance"] = 80,
-		["Price"] = 50000000,
-		["PurchaseID"] = 3312016651,
-		["LayoutOrder"] = 1,
-		["PetEggDescription"] = "It\'s a dog"
-	}
+    ["Common Egg"] = {
+        ["EggName"] = "Common Egg",
+        ["EggRarity"] = "Common",
+        ["StockChance"] = 1,
+        ["Price"] = 50000,
+        ["PurchaseID"] = 3276346455,
+        ["GiftPurchaseID"] = 3331486819,
+        ["LayoutOrder"] = 10,
+        ["PetEggDescription"] = "",
+        ["FallbackPrice"] = 19
+    },
+    ["Uncommon Egg"] = {
+        ["EggName"] = "Uncommon Egg",
+        ["EggRarity"] = "Uncommon",
+        ["StockChance"] = 6,
+        ["Price"] = 150000,
+        ["PurchaseID"] = 3276346509,
+        ["GiftPurchaseID"] = 3376050257,
+        ["LayoutOrder"] = 15,
+        ["PetEggDescription"] = "",
+        ["FallbackPrice"] = 39
+    },
+    ["Rare Egg"] = {
+        ["EggName"] = "Rare Egg",
+        ["EggRarity"] = "Rare",
+        ["StockChance"] = 20,
+        ["Price"] = 600000,
+        ["PurchaseID"] = 3276346557,
+        ["GiftPurchaseID"] = 3376050938,
+        ["LayoutOrder"] = 20,
+        ["PetEggDescription"] = "",
+        ["FallbackPrice"] = 89
+    },
+    ["Legendary Egg"] = {
+        ["EggName"] = "Legendary Egg",
+        ["EggRarity"] = "Legendary",
+        ["StockChance"] = 50,
+        ["Price"] = 3000000,
+        ["PurchaseID"] = 3276346676,
+        ["GiftPurchaseID"] = 3376048526,
+        ["LayoutOrder"] = 25,
+        ["PetEggDescription"] = "",
+        ["FallbackPrice"] = 129
+    },
+    ["Mythical Egg"] = {
+        ["EggName"] = "Mythical Egg",
+        ["EggRarity"] = "Mythical",
+        ["StockChance"] = 70,
+        ["Price"] = 8000000,
+        ["PurchaseID"] = 3286560171,
+        ["GiftPurchaseID"] = 3362615399,
+        ["LayoutOrder"] = 35,
+        ["PetEggDescription"] = "",
+        ["FallbackPrice"] = 119
+    },
+    ["Bug Egg"] = {
+        ["EggName"] = "Bug Egg",
+        ["EggRarity"] = "Divine",
+        ["StockChance"] = 100,
+        ["Price"] = 50000000,
+        ["PurchaseID"] = 3277000452,
+        ["GiftPurchaseID"] = 3362615986,
+        ["LayoutOrder"] = 60,
+        ["PetEggDescription"] = "",
+        ["FallbackPrice"] = 149
+    },
+    ["Common Summer Egg"] = {
+        ["EggName"] = "Common Summer Egg",
+        ["EggRarity"] = "Common",
+        ["StockChance"] = 12,
+        ["Price"] = 1000000,
+        ["PurchaseID"] = 3312016380,
+        ["GiftPurchaseID"] = 3362614532,
+        ["LayoutOrder"] = 15,
+        ["PetEggDescription"] = "",
+        ["FallbackPrice"] = 29
+    },
+    ["Rare Summer Egg"] = {
+        ["EggName"] = "Rare Summer Egg",
+        ["EggRarity"] = "Rare",
+        ["StockChance"] = 40,
+        ["Price"] = 25000000,
+        ["PurchaseID"] = 3312016506,
+        ["GiftPurchaseID"] = 3362614972,
+        ["LayoutOrder"] = 20,
+        ["PetEggDescription"] = "",
+        ["FallbackPrice"] = 99
+    },
+    ["Paradise Egg"] = {
+        ["EggName"] = "Paradise Egg",
+        ["EggRarity"] = "Mythical",
+        ["StockChance"] = 80,
+        ["Price"] = 50000000,
+        ["PurchaseID"] = 3312016651,
+        ["GiftPurchaseID"] = 3362615625,
+        ["LayoutOrder"] = 40,
+        ["PetEggDescription"] = "",
+        ["FallbackPrice"] = 139
+    }
 }
 return v2
