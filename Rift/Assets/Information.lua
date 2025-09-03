814c4ad0a6ce5d41dde79ed1acae3ad3e158777f
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-09-03 19:53:34 +0100

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -54,7 +54,7 @@ return {
             "<font color=\"#00ff00\">◉</font> <b>99 Nights in the Forest</b>\n" ..
             "<font color=\"#00ff00\">◉</font> <b>Dead Rails</b>\n" ..
             "<font color=\"#00ff00\">◉</font> <b>Forsaken</b>\n" ..
-            "<font color=\"#ff0000\">◉</font> <b>Ink Game</b>\n\nWe are working to update our emulator for Ink Game. Stay tuned!",
+            "<font color=\"#00ff00\">◉</font> <b>Ink Game</b>\n\nInk Game is back up and operational!",
     SCRIPT_NAMES = {
         "BetterBypasser (Chat Bypass)",
         "Rizzler"
