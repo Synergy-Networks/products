7a0242906fd17deea714c8d3a9c2b60b4a201105
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-09-06 17:51:21 +0100

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -54,7 +54,7 @@ return {
             "<font color=\"#00ff00\">◉</font> <b>99 Nights in the Forest</b>\n" ..
             "<font color=\"#00ff00\">◉</font> <b>Dead Rails</b>\n" ..
             "<font color=\"#00ff00\">◉</font> <b>Forsaken</b>\n" ..
-            "<font color=\"#00ff00\">◉</font> <b>Ink Game</b>\n\nRift is <font color="#DC551E"><b>KEYLESSL</b></font> for one day. Enjoy!",
+            '<font color=\"#00ff00\">◉</font> <b>Ink Game</b>\n\nRift is <font color="#DC551E"><b>KEYLESSL</b></font> for one day. Enjoy!',
     SCRIPT_NAMES = {
         "BetterBypasser (Chat Bypass)",
         "Rizzler"
