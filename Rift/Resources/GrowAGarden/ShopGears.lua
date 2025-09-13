be9462e6b2e40458cb2fb867be1659ed30b3241b
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-09-13 11:19:17 +0100

    Create ShopGears.lua

---
+++ Rift/Resources/GrowAGarden/ShopGears.lua
@@ -0,0 +1,170 @@
+local v4 = {
+    ["Watering Can"] = {
+        ["StockChance"] = 1,
+        ["StockAmount"] = { 1, 3 },
+        ["Price"] = 50000,
+        ["PurchaseID"] = 3260229242,
+        ["GiftPurchaseID"] = 3269349334,
+        ["LayoutOrder"] = 10,
+        ["FallbackPrice"] = 39
+    },
+    ["Trading Ticket"] = {
+        ["StockChance"] = 1,
+        ["StockAmount"] = { 1, 2 },
+        ["Price"] = 100000,
+        ["PurchaseID"] = 3352931398,
+        ["GiftPurchaseID"] = 3352931957,
+        ["DisplayInShop"] = true,
+        ["FallbackPrice"] = 19,
+        ["LayoutOrder"] = 15
+    },
+    ["Trowel"] = {
+        ["Gear"] = v2.Trowel,
+        ["StockChance"] = 1,
+        ["StockAmount"] = { 1, 3 },
+        ["Price"] = 100000,
+        ["PurchaseID"] = 3265946561,
+        ["Stack"] = 5,
+        ["DisplayInShop"] = true,
+        ["LayoutOrder"] = 20,
+        ["FallbackPrice"] = 49
+    },
+    ["Recall Wrench"] = {
+        ["StockChance"] = 1,
+        ["StockAmount"] = { 1, 3 },
+        ["Price"] = 150000,
+        ["PurchaseID"] = 3282918403,
+        ["GiftPurchaseID"] = 3282918955,
+        ["LayoutOrder"] = 30,
+        ["GearDescription"] = "Teleports to Gear Shop. 5 uses",
+        ["FallbackPrice"] = 59
+    },
+    ["Basic Sprinkler"] = {
+        ["StockChance"] = 3,
+        ["StockAmount"] = { 1, 3 },
+        ["Price"] = 25000,
+        ["PurchaseID"] = 3265889601,
+        ["GiftPurchaseID"] = 3269349445,
+        ["LayoutOrder"] = 40,
+        ["FallbackPrice"] = 79
+    },
+    ["Advanced Sprinkler"] = {
+        ["StockChance"] = 7,
+        ["StockAmount"] = { 1, 2 },
+        ["Price"] = 50000,
+        ["PurchaseID"] = 3265889751,
+        ["GiftPurchaseID"] = 3269349768,
+        ["LayoutOrder"] = 50,
+        ["FallbackPrice"] = 99
+    },
+    ["Medium Toy"] = {
+        ["StockChance"] = 50,
+        ["StockAmount"] = { 1, 2 },
+        ["Price"] = 4000000,
+        ["PurchaseID"] = 3329526686,
+        ["GiftPurchaseID"] = 3329530133,
+        ["LayoutOrder"] = 53,
+        ["FallbackPrice"] = 189
+    },
+    ["Medium Treat"] = {
+        ["StockChance"] = 50,
+        ["StockAmount"] = { 1, 2 },
+        ["Price"] = 4000000,
+        ["PurchaseID"] = 3329527365,
+        ["GiftPurchaseID"] = 3329529928,
+        ["LayoutOrder"] = 57,
+        ["FallbackPrice"] = 119
+    },
+    ["Godly Sprinkler"] = {
+        ["StockChance"] = 11,
+        ["StockAmount"] = { 1, 1 },
+        ["Price"] = 120000,
+        ["PurchaseID"] = 3265889948,
+        ["GiftPurchaseID"] = 3269349908,
+        ["LayoutOrder"] = 60,
+        ["FallbackPrice"] = 149
+    },
+    ["Magnifying Glass"] = {
+        ["StockChance"] = 3,
+        ["StockAmount"] = { 1, 1 },
+        ["Price"] = 10000000,
+        ["PurchaseID"] = 3316261725,
+        ["GiftPurchaseID"] = 3317955209,
+        ["LayoutOrder"] = 80,
+        ["FallbackPrice"] = 99
+    },
+    ["Master Sprinkler"] = {
+        ["StockChance"] = 100,
+        ["StockAmount"] = { 1, 1 },
+        ["Price"] = 10000000,
+        ["PurchaseID"] = 3267580365,
+        ["GiftPurchaseID"] = 3269350167,
+        ["LayoutOrder"] = 100,
+        ["FallbackPrice"] = 199
+    },
+    ["Cleaning Spray"] = {
+        ["StockChance"] = 3,
+        ["StockAmount"] = { 1, 2 },
+        ["Price"] = 15000000,
+        ["PurchaseID"] = 3306767043,
+        ["GiftPurchaseID"] = 3306767451,
+        ["LayoutOrder"] = 110,
+        ["FallbackPrice"] = 139
+    },
+    ["Favorite Tool"] = {
+        ["GearRarity"] = "Divine",
+        ["StockChance"] = 1,
+        ["StockAmount"] = { 1, 3 },
+        ["Price"] = 20000000,
+        ["PurchaseID"] = 3281679093,
+        ["GiftPurchaseID"] = 3281679185,
+        ["LayoutOrder"] = 120,
+        ["FallbackPrice"] = 119
+    },
+    ["Harvest Tool"] = {
+        ["StockChance"] = 1,
+        ["StockAmount"] = { 1, 3 },
+        ["Price"] = 30000000,
+        ["PurchaseID"] = 3286038236,
+        ["GiftPurchaseID"] = 3286038078,
+        ["LayoutOrder"] = 130,
+        ["FallbackPrice"] = 149
+    },
+    ["Friendship Pot"] = {
+        ["StockChance"] = 50,
+        ["StockAmount"] = { 1, 1 },
+        ["Price"] = 15000000,
+        ["PurchaseID"] = 3301473650,
+        ["GiftPurchaseID"] = 3301474767,
+        ["LayoutOrder"] = 140,
+        ["FallbackPrice"] = 39
+    },
+    ["Grandmaster Sprinkler"] = {
+        ["StockChance"] = 200,
+        ["StockAmount"] = { 1, 1 },
+        ["Price"] = 1000000000,
+        ["PurchaseID"] = 3354130076,
+        ["GiftPurchaseID"] = 3354131020,
+        ["LayoutOrder"] = 160,
+        ["FallbackPrice"] = 279
+    },
+    ["Levelup Lollipop"] = {
+        ["StockChance"] = 300,
+        ["StockAmount"] = { 1, 2 },
+        ["Price"] = 10000000000,
+        ["PurchaseID"] = 3329528135,
+        ["GiftPurchaseID"] = 3329530334,
+        ["LayoutOrder"] = 170,
+        ["FallbackPrice"] = 79
+    },
+    ["Cleansing Pet Shard"] = {
+        ["StockChance"] = 12,
+        ["StockAmount"] = { 1, 1 },
+        ["Price"] = 30000000,
+        ["PurchaseID"] = 3379245220,
+        ["GiftPurchaseID"] = 3379255032,
+        ["LayoutOrder"] = 115,
+        ["FallbackPrice"] = 139
+    }
+}
+return v4
