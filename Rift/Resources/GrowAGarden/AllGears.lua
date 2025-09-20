8607e57a0a26e55d588aeccba574a01180850661
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-09-20 13:47:52 +0100

    Update AllGears.lua

--- Rift/Resources/GrowAGarden/AllGears.lua
+++ Rift/Resources/GrowAGarden/AllGears.lua
@@ -4,7 +4,6 @@ local v3 = {
         ["GearRarity"] = "Common",
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://140223014467344",
-        ["GearDescription"] = "Speeds up plant growth. 10 uses"
     },
     ["Trading Ticket"] = {
         ["GearName"] = "Trading Ticket",
@@ -17,70 +16,60 @@ local v3 = {
         ["GearRarity"] = "Uncommon",
         ["Stack"] = 5,
         ["Asset"] = "rbxassetid://115261280019001",
-        ["GearDescription"] = "Moves plants. 5 uses"
     },
     ["Recall Wrench"] = {
         ["GearName"] = "Recall Wrench",
         ["GearRarity"] = "Uncommon",
         ["Stack"] = 5,
         ["Asset"] = "rbxassetid://98327818593168",
-        ["GearDescription"] = "Teleports to Gear Shop. 5 uses"
     },
     ["Basic Sprinkler"] = {
         ["GearName"] = "Basic Sprinkler",
         ["GearRarity"] = "Rare",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://120314005864914",
-        ["GearDescription"] = "Increases growth speed and fruit size. Lasts 5 minutes"
     },
     ["Advanced Sprinkler"] = {
         ["GearName"] = "Advanced Sprinkler",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://90193997839883",
-        ["GearDescription"] = "Increases growth speed and mutation chances. Lasts 5 minutes"
     },
     ["Small Toy"] = {
         ["GearName"] = "Small Toy",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://118990580091132",
-        ["GearDescription"] = "Give your pet a small boost to their passive ability"
     },
     ["Medium Toy"] = {
         ["GearName"] = "Medium Toy",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 3,
         ["Asset"] = "rbxassetid://87196351819335",
-        ["GearDescription"] = "Give your pet a medium boost to their passive ability"
     },
     ["Small Treat"] = {
         ["GearName"] = "Small Treat",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://107646462537237",
-        ["GearDescription"] = "Give your pet a small boost to their xp over time"
     },
     ["Medium Treat"] = {
         ["GearName"] = "Medium Treat",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 3,
         ["Asset"] = "rbxassetid://134834545436849",
-        ["GearDescription"] = "Give your pet a medium boost to their xp over time"
     },
     ["Godly Sprinkler"] = {
         ["GearName"] = "Godly Sprinkler",
         ["GearRarity"] = "Mythical",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://75442797836082",
-        ["GearDescription"] = "Increases growth speed, mutation chances and fruit size. Lasts 5 minutes"
     },
     ["Lightning Rod"] = {
         ["GearName"] = "Lightning Rod",
         ["GearRarity"] = "Mythical",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://125433095334047",
-        ["GearDescription"] = "Redirects lightning 3 times before being destroyed"
     },
     ["Magnifying Glass"] = {
         ["GearName"] = "Magnifying Glass",
@@ -189,91 +178,78 @@ local v3 = {
         ["GearRarity"] = "Rare",
         ["Stack"] = 5,
         ["Asset"] = "rbxassetid://120464955669662",
-        ["GearDescription"] = "Teleports to the event!"
     },
     ["Small Bowl"] = {
         ["GearName"] = "Small Pet Size Boost",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://114089908282697",
-        ["GearDescription"] = "Give your pet a small boost to their size and weight"
     },
     ["Medium Bowl"] = {
         ["GearName"] = "Medium Pet Size Boost",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 3,
         ["Asset"] = "rbxassetid://127973837115000",
-        ["GearDescription"] = "Give your pet a medium boost to their size and weight"
     },
     ["Garden Guide"] = {
         ["GearName"] = "Garden Guide",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://96147201616033",
-        ["GearDescription"] = "A book containing information about potential plants!"
     },
     ["Plant Booster"] = {
         ["GearName"] = "Plant Booster",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://86602672756084",
-        ["GearDescription"] = "A plant booster"
     },
     ["Pet Shard Tranquil"] = {
         ["GearName"] = "Pet Shard Tranquil",
         ["GearRarity"] = "Divine",
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://89240663276696",
-        ["GearDescription"] = "Calm your pet\'s mind and make it tranquil!"
     },
     ["Pet Shard Corrupted"] = {
         ["GearName"] = "Pet Shard Corrupted",
         ["GearRarity"] = "Divine",
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://114953192928342",
-        ["GearDescription"] = "Darken your pet\'s mind and make it corrupted!"
     },
     ["Pet Shard Fried"] = {
         ["GearName"] = "Pet Shard Fried",
         ["GearRarity"] = "Divine",
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://76156216865979",
-        ["GearDescription"] = "Unlock the inner chef in your pet!"
     },
     ["Pet Shard Mega"] = {
         ["GearName"] = "Pet Shard Mega",
         ["GearRarity"] = "Divine",
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://132251713933337",
-        ["GearDescription"] = "Make your pet MEGA like giants!"
     },
     ["Pet Shard Aromatic"] = {
         ["GearName"] = "Pet Shard Aromatic",
         ["GearRarity"] = "Divine",
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://126438230205950",
-        ["GearDescription"] = "Smells like it\'s fresh out the oven"
     },
     ["Pet Shard Silver"] = {
         ["GearName"] = "Pet Shard Silver",
         ["GearRarity"] = "Divine",
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://121577824688085",
-        ["GearDescription"] = "Polish up your pet and make it silver!"
     },
     ["Pet Shard Golden"] = {
         ["GearName"] = "Pet Shard Golden",
         ["GearRarity"] = "Divine",
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://93203116407679",
-        ["GearDescription"] = "Make your pet shine like the sun!"
     },
     ["Pet Shard Rainbow"] = {
         ["GearName"] = "Pet Shard Rainbow",
         ["GearRarity"] = "Divine",
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://103557510521587",
-        ["GearDescription"] = "Make your pet shine bright in rainbow light!"
     },
     ["Cleansing Pet Shard"] = {
         ["GearName"] = "Cleansing Pet Shard",
@@ -281,42 +257,36 @@ local v3 = {
         ["StockChance"] = 12,
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://80815612016706",
-        ["GearDescription"] = "Bring your pet back to normal!"
     },
     ["Pet Shard GiantBean"] = {
         ["GearName"] = "Pet Shard GiantBean",
         ["GearRarity"] = "Divine",
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://99321086204906",
-        ["GearDescription"] = "Make your pet turn into a Giant Bean!"
     },
     ["Pet Shard Glimmering"] = {
         ["GearName"] = "Pet Shard Glimmering",
         ["GearRarity"] = "Divine",
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://125506626001248",
-        ["GearDescription"] = "Make your pet glimmer like a fairy!"
     },
     ["Pet Shard Luminous"] = {
         ["GearName"] = "Pet Shard Luminous",
         ["GearRarity"] = "Divine",
         ["Stack"] = 10,
         ["Asset"] = "rbxassetid://125506626001248",
-        ["GearDescription"] = "Make your pet luminous!"
     },
     ["Tranquil Radar"] = {
         ["GearName"] = "Tranquil Radar",
         ["GearRarity"] = "Common",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://133779065360759",
-        ["GearDescription"] = ""
     },
     ["Corrupt Radar"] = {
         ["GearName"] = "Corrupt Radar",
         ["GearRarity"] = "Common",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://101384658792701",
-        ["GearDescription"] = ""
     },
     ["Glimmering Radar"] = {
         ["GearName"] = "Glimmering Radar",
@@ -449,70 +419,60 @@ local v3 = {
         ["GearRarity"] = "Common",
         ["Stack"] = 5,
         ["Asset"] = "rbxassetid://106727369137782",
-        ["GearDescription"] = "Launches fireworks! 5 Uses"
     },
     ["Beanworks"] = {
         ["GearName"] = "Beanworks",
         ["GearRarity"] = "Common",
         ["Stack"] = 5,
         ["Asset"] = "rbxassetid://106727369137782",
-        ["GearDescription"] = "Launches Beanstalk fireworks! 5 Uses"
     },
     ["Tropical Mist Sprinkler"] = {
         ["GearName"] = "Tropical Mist Sprinkler",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://122592465315585",
-        ["GearDescription"] = "Increases growth speed and size bonus for Coconut, Dragon Fruit, Mango, Cacao & Watermelon. Lasts 4 minutes"
     },
     ["Spice Spritzer Sprinkler"] = {
         ["GearName"] = "Spice Spritzer Sprinkler",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://130541096369176",
-        ["GearDescription"] = "Greatly increases variant chance bonus for Pepper, Cacao, Ember Lily and Papaya. Lasts 2.5 minutes"
     },
     ["Sweet Soaker Sprinkler"] = {
         ["GearName"] = "Sweet Soaker Sprinkler",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://91036428046936",
-        ["GearDescription"] = "Increases size bonus for Watermelon, Pumpkin and Moon Melon. Lasts 5 minutes"
     },
     ["Stalk Sprout Sprinkler"] = {
         ["GearName"] = "Stalk Sprout Sprinkler",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://125326661954614",
-        ["GearDescription"] = "Greatly increases growth speed and wets Beanstalk, Bamboo and Mushroom. Lasts 2 minutes"
     },
     ["Berry Blusher Sprinkler"] = {
         ["GearName"] = "Berry Blusher Sprinkler",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://121776369197466",
-        ["GearDescription"] = "Greatly increases size bonus for Grape, Blueberry, Strawberry, Raspberry and Celestiberry. Lasts 1.5 minutes"
     },
     ["Flower Froster Sprinkler"] = {
         ["GearName"] = "Flower Froster Sprinkler",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://86602672756084",
-        ["GearDescription"] = "Freezes Flower type plants. Lasts 2.5 minutes"
     },
     ["Bean Speaker"] = {
         ["GearName"] = "Bean Speaker",
         ["GearRarity"] = "Common",
         ["Stack"] = 5,
         ["Asset"] = "rbxassetid://106727369137782",
-        ["GearDescription"] = "Bean or leave!"
     },
     ["Can Of Beans"] = {
         ["GearName"] = "Can Of Beans",
         ["GearRarity"] = "Common",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://75302189671253",
-        ["GearDescription"] = "For when you need a little boost in life..."
     },
     ["Fairy Summoner"] = {
         ["GearName"] = "Fairy Summoner",
@@ -561,111 +521,95 @@ local v3 = {
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://75965661010163",
-        ["GearDescription"] = "Picks a new random name for your pet!"
     },
     ["Pet Lead"] = {
         ["GearName"] = "Pet Lead",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://129795023623532",
-        ["GearDescription"] = "Make a pet follow you for 10 seconds!"
     },
     ["Chocolate Sprinkler"] = {
         ["GearName"] = "Chocolate Sprinkler",
         ["GearRarity"] = "Mythical",
         ["Asset"] = "rbxassetid://115881753102059",
         ["Stack"] = 1,
-        ["GearDescription"] = "Covers plants in chocolate! Lasts 1 minute"
     },
     ["Nectar Staff"] = {
         ["GearName"] = "Nectar Staff",
         ["GearRarity"] = "Common",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://109105197997415",
-        ["GearDescription"] = ""
     },
     ["Night Staff"] = {
         ["GearName"] = "Night Staff",
         ["GearRarity"] = "Common",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://86894493095285",
-        ["GearDescription"] = ""
     },
     ["Tranquil Staff"] = {
         ["GearName"] = "Tranquil Staff",
         ["GearRarity"] = "Common",
         ["Asset"] = "rbxassetid://118265137979088",
-        ["GearDescription"] = ""
     },
     ["Corrupt Staff"] = {
         ["GearName"] = "Corrupt Staff",
         ["GearRarity"] = "Common",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://133948006225272",
-        ["GearDescription"] = ""
     },
     ["Star Caller"] = {
         ["GearName"] = "Star Caller",
         ["GearRarity"] = "Common",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://108537992961712",
-        ["GearDescription"] = ""
     },
     ["Honey Sprinkler"] = {
         ["GearName"] = "Honey Sprinkler",
         ["GearRarity"] = "Divine",
         ["Asset"] = "rbxassetid://90156288104343",
         ["Stack"] = 1,
-        ["GearDescription"] = "Covers plants in Honey! Lasts 1 minute"
     },
     ["Pollen Radar"] = {
         ["GearName"] = "Pollen Radar",
         ["GearRarity"] = "Common",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://105620836609339",
-        ["GearDescription"] = ""
     },
     ["Firefly Jar"] = {
         ["GearName"] = "Firefly Jar",
         ["GearRarity"] = "Rare",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://123147701320566",
-        ["GearDescription"] = "A firefly in a jar!"
     },
     ["Sky Lantern"] = {
         ["GearName"] = "Sky Lantern",
         ["GearRarity"] = "Uncommon",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://88840697754774",
-        ["GearDescription"] = "Light up the skies with the glow of fall"
     },
     ["Maple Leaf Kite"] = {
         ["GearName"] = "Maple Leaf Kite",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://94694559446067",
-        ["GearDescription"] = "Look at it soar!"
     },
     ["Leaf Blower"] = {
         ["GearName"] = "Leaf Blower",
         ["GearRarity"] = "Divine",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://116067282320680",
-        ["GearDescription"] = "Fly with the leaves"
     },
     ["Maple Syrup"] = {
         ["GearName"] = "Maple Syrup",
         ["GearRarity"] = "Legendary",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://82575496704431",
-        ["GearDescription"] = "Gives 50 XP to a pet!"
     },
     ["Maple Leaf Charm"] = {
         ["GearName"] = "Maple Leaf Charm",
         ["GearRarity"] = "Divine",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://132566474931424",
-        ["GearDescription"] = "Summons the winds to bring forth rain."
     },
     ["Maple Sprinkler"] = {
         ["GearName"] = "Maple Sprinkler",
@@ -678,21 +622,66 @@ local v3 = {
         ["GearRarity"] = "Prismatic",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://71754335799002",
-        ["GearDescription"] = "Place down to give a temporary boost for cooking and fall-related pets. Boost decreases as fire weakens"
     },
     ["Harvest Basket"] = {
         ["GearName"] = "Harvest Basket",
         ["GearRarity"] = "Prismatic",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://90267962738984",
-        ["GearDescription"] = "Toss a bunch of fruit around! Feeds all your pets"
     },
     ["Golden Acorn"] = {
         ["GearName"] = "Golden Acorn",
         ["GearRarity"] = "Prismatic",
         ["Stack"] = 1,
         ["Asset"] = "rbxassetid://122041101537564",
-        ["GearDescription"] = "Resets a pet back to Age 1 but empowers it with a random mutation! (Must be over Age 40)"
+    },
+    ["Rake"] = {
+        ["GearName"] = "Rake",
+        ["GearRarity"] = "Divine",
+        ["Stack"] = 1,
+        ["Asset"] = "rbxassetid://123007566196057",
+    },
+    ["Sheckles Gun"] = {
+        ["GearName"] = "Sheckles Gun",
+        ["GearRarity"] = "Divine",
+        ["Stack"] = 1,
+        ["Asset"] = "rbxassetid://128722800183998",
+    },
+    ["Super Leaf Blower"] = {
+        ["GearName"] = "Super Leaf Blower",
+        ["GearRarity"] = "Prismatic",
+        ["Stack"] = 1,
+        ["Asset"] = "rbxassetid://115901959792916",
+    },
+    ["Acorn Bell"] = {
+        ["GearName"] = "Acorn Bell",
+        ["GearRarity"] = "Divine",
+        ["Stack"] = 1,
+        ["Asset"] = "rbxassetid://109503976464120",
+    },
+    ["Acorn Lollipop"] = {
+        ["GearName"] = "Acorn Lollipop",
+        ["GearRarity"] = "Prismatic",
+        ["Stack"] = 1,
+        ["Asset"] = "rbxassetid://114980305116277",
+    },
+    ["Grow All"] = {
+        ["GearName"] = "Grow All",
+        ["GearRarity"] = "Common",
+        ["Stack"] = 1,
+        ["Asset"] = "rbxassetid://120928632087334",
+    },
+    ["Steal"] = {
+        ["GearName"] = "Steal",
+        ["GearRarity"] = "Common",
+        ["Stack"] = 1,
+        ["Asset"] = "rbxassetid://71833009041293",
+    },
+    ["Super Watering Can"] = {
+        ["GearName"] = "Super  Watering Can",
+        ["GearRarity"] = "Common",
+        ["Stack"] = 1,
+        ["Asset"] = "rbxassetid://139084715000316",
     }
 }
 return v3
