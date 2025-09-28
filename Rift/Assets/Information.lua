788c3c9964e629af1e87bf58c30943405b10a44c
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-09-28 19:40:06 +0100

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -43,7 +43,7 @@ end
 
 
 return {
-    VERSION = "1.25.22.20",
+    VERSION = "1.25.22.21",
     CHANNEL = "Beta",
     ADVERTISEMENT_ENABLED = false,
     CHANGELOGS = Changelogs .. "\n\nThis update log may not be up to date!",
