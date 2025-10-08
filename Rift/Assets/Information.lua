36cba90e2203279b71b6322d0e8509842f2f0c48
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-10-08 23:24:07 +0100

    Update Information.lua

--- Rift/Assets/Information.lua
+++ Rift/Assets/Information.lua
@@ -45,7 +45,7 @@ end
 return {
     VERSION = "1.25.23.3",
     CHANNEL = "Beta",
-    ADVERTISEMENT_ENABLED = true,
+    ADVERTISEMENT_ENABLED = false,
     CHANGELOGS = Changelogs .. "\n\nThis update log may not be up to date!",
     GAMES_SUPPORTED =  "<font color=\"#00ff00\">◉</font> → Maintained & Updated\n" ..
             "<font color=\"#ffff00\">◉</font> → Experimental / Has Issues\n" ..
